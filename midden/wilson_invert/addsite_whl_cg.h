5494a3fb308135495d6a368a1ff6e9d7666271b0
Carleton DeTar <detar@physics.utah.edu>
2005-02-23 00:36:12 +0000

    Housekeeping

--- midden/wilson_invert/addsite_whl_cg.h
+++
@@ -1,2 +0,0 @@
-/* Addition to site structure for mixed flavor (heavy-light) code */
-	wilson_propagator quark_prop2;
--- midden/wilson_invert/addsite_wil_bi.h
+++
@@ -1,3 +0,0 @@
-/* Addition to site structure for biconjugate gradient inverter */
-	wilson_vector sss;	/* internal biconjugate gradient vector */
-	wilson_vector tmp;	/* auxiliary for other internal biCG vectors */
