dcbc3061029ea7fe8de94771a060fafbd88f6820
Carleton DeTar <detar@physics.utah.edu>
2005-03-27 18:55:03 +0000

    Removing obsolete files

--- ludmila/ks_hl_spectrum/defines.h
+++
@@ -1,9 +0,0 @@
-#ifndef _DEFINES_H
-#define _DEFINES_H
-
-/* Compiler macros common to all targets in this application */
-
-#define SITERAND	/* Use site-based random number generators */
-#define GAUGE_FIX_TOL 2.0e-6
-#define MAXHTMP 8
-#endif /* _DEFINES_H */
