c8f88e6873e6be92429181db842e8a579657d6d8
Carleton DeTar <detar@physics.utah.edu>
2005-03-27 14:38:21 +0000

    Take input from stdin

---
+++ file_utilities/wprop_info.c
@@ -0,0 +1,83 @@
+/*********************** ksprop_info.c *************************/
+/* MIMD version 6 */
+
+/* For ks_imp_invert_multi */
+
+/* Application-dependent routine for writing ksprop info file */
+/* This file is an ASCII companion to the KS propagator file
+   and contains information about the action used to generate it.
+   This information is consistently written in the pattern
+
+       keyword  value
+
+   or
+
+       keyword[n] value1 value2 ... valuen
+
+   where n is an integer.
+
+   To maintain a semblance of consistency, the possible keywords are
+   listed in io_wprop.h.
+
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "lattice.h"
+#include "../include/io_wprop.h"
+#define MAX_XML 2049
+
+/*---------------------------------------------------------------------------*/
+
+/* Fill in the spin table of contents for the propagator header -
+   In some projects we may want to write propagators for only
+   a couple of source spins, rather than the complete set of 4.
+   This table of contents specifies which spin values actually
+   appear. */
+
+void build_w_prop_hdr(w_prop_header *wph)
+{
+  int i;
+
+  /* Note that all other values in the header structure are
+     loaded by the io_prop_w.c routines, since they are common
+     to all projects */
+
+  /* Copy from values preset in lattice_cl.h */
+
+  wph->n_spins  = n_spins;
+  for(i=0;i<n_spins;i++)
+    wph->spins[i] = spins[i];
+
+} /* build_w_prop_hdr */
+
+/*---------------------------------------------------------------------------*/
+/* This routine writes the ASCII info file.  It is called from one of
+   the lattice output routines in ../generic_w/io_prop_w.c. */
+
+void write_appl_w_prop_info(FILE *fp)
+{
+
+  /* Note that the file has already been opened and
+     the required magic number, time stamp, and lattice
+     dimensions have already been written */
+
+  /* The rest are optional */
+
+}
+
+/* Temporary until we can create XML */
+/* TO DO: Proviso for a number of quark action paths different from 6 */
+char *create_w_XML()
+{
+  char *xml;
+
+  xml = (char *)malloc(MAX_XML);
+  strcpy(xml, "Wilson Propagator");
+  return xml;
+}
+
+
+void free_w_XML(char *xml){
+  if(xml != NULL)free(xml);
+}
--- file_utilities/wprop_to_wprop.c
+++ file_utilities/wprop_to_wprop.c
@@ -1,11 +1,11 @@
-/*************************** ksprop_to_scidac.c ************************/
+/*************************** wprop_to_scidac.c ************************/
 /* MIMD version 6 */
-/* Read a KS prop, convert to SciDAC format */
-/* C. DeTar 3/22/05 */
+/* Read a Wilson prop, convert to SciDAC format */
+/* C. DeTar 3/25/05 */
 
 /* Usage ...
 
-   ksprop_to_scidac milc_file scidac_file
+   wprop_to_scidac milc_file scidac_file
 
 */
 
@@ -22,18 +22,19 @@
 #include <time.h>
 #include <string.h>
 #include "../include/file_types.h"
-#include "../include/io_ksprop.h"
+#include "../include/io_wprop.h"
 #include "../include/generic.h"
-#include "../include/generic_ks.h"
+#include "../include/generic_wilson.h"
 #include "../include/io_lat.h"
 #include <qio.h>
 
 #define MAX_RECXML 512
 
-static file_type ksprop_list[N_KSPROP_TYPES] =
-  { {FILE_TYPE_KSPROP,       KSPROP_VERSION_NUMBER},
-    {FILE_TYPE_KSFMPROP,     KSFMPROP_VERSION_NUMBER},
-    {FILE_TYPE_KSQIOPROP,    LIME_MAGIC_NO}
+static file_type w_prop_list[N_WPROP_TYPES] =
+  { {FILE_TYPE_W_PROP,       W_PROP_VERSION_NUMBER},
+    {FILE_TYPE_W_PROP_1996,  W_PROP_VERSION_NUMBER_1996},
+    {FILE_TYPE_W_FMPROP,     W_FMPROP_VERSION_NUMBER},
+    {FILE_TYPE_W_QIOPROP,    LIME_MAGIC_NO}
   };
 
 /*----------------------------------------------------------------------*/
@@ -75,7 +76,33 @@ void free_lattice()
 }
 /*----------------------------------------------------------------------*/
 
-void setup() {
+int get_prompt( int *prompt ){
+    char initial_prompt[80];
+    int status;
+
+    *prompt = -1;
+    printf( "type 0 for no prompts  or 1 for prompts\n");
+    status = scanf("%s",initial_prompt);
+    if(status != 1){
+      printf("\nget_prompt: Can't read stdin\n");
+      terminate(1);
+    }
+    if(strcmp(initial_prompt,"prompt") == 0)  {
+       scanf("%d",prompt);
+    }
+    else if(strcmp(initial_prompt,"0") == 0) *prompt=0;
+    else if(strcmp(initial_prompt,"1") == 0) *prompt=1;
+
+    if( *prompt==0 || *prompt==1 )return(0);
+    else{
+        printf("\nget_prompt: ERROR IN INPUT: initial prompt\n");
+        return(1);
+    }
+}
+
+/*----------------------------------------------------------------------*/
+
+void setup_refresh() {
 
   /* Set up lattice */
   broadcast_bytes((char *)&nx,sizeof(int));
@@ -108,11 +135,12 @@ int readin(prompt)
 
   if(this_node==0) {
 
-    IF_OK status += ask_starting_ksprop( prompt, &(par_buf.startflag),
-					 par_buf.startfile );
+    IF_OK status += ask_starting_wprop( prompt, &(par_buf.startflag),
+					par_buf.startfile );
     /* find out what to do with lattice at end */
-    IF_OK status += ask_ending_ksprop( prompt, &(par_buf.saveflag),
-				       par_buf.savefile );
+    IF_OK status += ask_ending_wprop( prompt, &(par_buf.saveflag),
+				      par_buf.savefile );
+    if(status > 0)par_buf.stopflag = 1; else par_buf.stopflag = 0;
   }
 
   /* Node 0 broadcasts parameter buffer to all other nodes */
@@ -128,27 +156,30 @@ int main(int argc, char *argv[])
 
   int file_type;
   char recxml[MAX_RECXML];
-  int i;
   int dims[4],ndim;
-  su3_vector *ksprop;
+  int prompt;
+  wilson_propagator *wprop;
 
   initialize_machine(argc,argv);
 
   this_node = mynode();
   number_of_nodes = numnodes();
 
+  if(get_prompt(&prompt) != 0)
+    return 0;
+
   /* Loop over input requests */
-  while(readin(1) == 0)
+  while(readin(prompt) == 0)
     {
       /* Sniff out the input file type */
-      file_type = io_detect(par_buf.startfile, ksprop_list, N_KSPROP_TYPES);
+      file_type = io_detect(par_buf.startfile, w_prop_list, N_WPROP_TYPES);
       if(file_type < 0){
 	node0_printf("Can't read file %s\n", par_buf.startfile);
 	return 1;
       }
-
+      
       /* Get the lattice dimensions from the input file */
-      read_lat_dim_ksprop(par_buf.startfile, file_type, &ndim, dims);
+      read_lat_dim_wprop(par_buf.startfile, file_type, &ndim, dims);
       
       if(this_node == 0)
 	{
@@ -157,31 +188,31 @@ int main(int argc, char *argv[])
 	}
 
       volume=nx*ny*nz*nt;
-
-      /* Finish setup - broadcast dimensions */
-      setup();
       
-      /* Allocate space for ksprop */
-      ksprop = (su3_vector *)malloc(sites_on_node*3*sizeof(su3_vector));
-
-      if(ksprop == NULL){
+      /* Finish setup - broadcast dimensions */
+      setup_refresh();
+      
+      /* Allocate space for the full propagator on each node */
+      wprop = (wilson_propagator *)malloc(sites_on_node*
+					  sizeof(wilson_propagator));
+      if(wprop == NULL){
 	node0_printf("No room for propagator\n");
 	terminate(1);
       }
-
+      
       if(this_node == 0)printf("Converting file %s to file %s\n",
-			   par_buf.startfile, par_buf.savefile);
-
-      /* Read the whole file */
-      reload_ksprop_to_field(par_buf.startflag, par_buf.startfile, ksprop, 0);
-
+			       par_buf.startfile, par_buf.savefile);
+      
+      /* Read the whole propagator */
+      reload_wprop_to_field(par_buf.startflag, par_buf.startfile, wprop, 1);
+      
       /* Write the whole propagator */
       /* Some arbitrary metadata */
       snprintf(recxml,MAX_RECXML,"Converted from %s",par_buf.startfile);
+      save_wprop_from_field(par_buf.saveflag, par_buf.savefile, recxml, 
+			    wprop, 1);
 
-      save_ksprop_from_field(par_buf.saveflag, par_buf.savefile, recxml, 
-			     ksprop, 1);
-      free(ksprop);
+      free(wprop);
       free_lattice();
     }
 
