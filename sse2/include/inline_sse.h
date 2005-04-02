f8ed1f146e6ee814684ae235e102479683b1cdde
Carleton DeTar <detar@physics.utah.edu>
2005-04-02 22:41:17 +0000

    Moved macro replacement to su3.h

--- sse2/include/inline_sse.h
+++ sse2/include/inline_sse.h
@@ -19,28 +19,6 @@
 /** #include "sse_s_m_a_vec.h" **/
 /** #include "sse_s_m_a_mat.h" **/
 
-#if defined SSE_INLINE
-
-#define mult_su3_nn(...) _inline_sse_mult_su3_nn(__VA_ARGS__)
-#define mult_su3_na(...) _inline_sse_mult_su3_na(__VA_ARGS__)
-#define mult_su3_an(...) _inline_sse_mult_su3_an(__VA_ARGS__)
-#define mult_su3_mat_vec(...) _inline_sse_mult_su3_mat_vec(__VA_ARGS__)
-#define mult_adj_su3_mat_vec(...) _inline_sse_mult_adj_su3_mat_vec(__VA_ARGS__)
-#define mult_su3_mat_vec_sum_4dir(...) _inline_sse_mult_su3_mat_vec_sum_4dir(__VA_ARGS__)
-#define mult_adj_su3_mat_vec_4dir(...) _inline_sse_mult_adj_su3_mat_vec_4dir(__VA_ARGS__)
-#define mult_adj_su3_mat_4vec(...) _inline_sse_mult_adj_su3_mat_4vec(__VA_ARGS__)
-#define su3_projector(...) _inline_sse_su3_projector(__VA_ARGS__)
-#define mult_su3_mat_hwvec(...) _inline_sse_mult_su3_mat_hwvec(__VA_ARGS__)
-#define mult_adj_su3_mat_hwvec(...) _inline_sse_mult_adj_su3_mat_hwvec(__VA_ARGS__)
-
-/** NOT AVAILABLE, YET **/
-/** #define sub_four_su3_vecs(...) _inline_sse_sub_four_su3_vecs(__VA_ARGS__) **/
-/** #define add_su3_vector(...) _inline_sse_add_su3_vector(__VA_ARGS__) **/
-/** #define scalar_mult_add_su3_vector(a,b,c,d) {double _temp = c; _inline_sse_scalar_mult_add_su3_vector(a,b,_temp,d);} **/
-/** #define scalar_mult_add_su3_matrix(a,b,c,d) {double _temp = c; _inline_sse_scalar_mult_add_su3_matrix(a,b,_temp,d);} **/
-
-#endif
-
 /* Corrections by A. Alexandru */
 typedef struct
 {
