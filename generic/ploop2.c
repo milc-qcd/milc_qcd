48e80497998550135f027e0fd7b2a2c1a6207dc3
Carleton DeTar <detar@physics.utah.edu>
2005-04-12 03:45:11 +0000

    Convert tempmat2 to malloc'ed field

--- generic/ploop2.c
+++ generic/ploop2.c
@@ -6,45 +6,52 @@
 #include "generic_includes.h"
 
 complex ploop() {
-register int i,t;
-register site *st;
-msg_tag *tag;
-complex sum;
-complex plp;
+  register int i,t;
+  register site *st;
+  msg_tag *tag;
+  complex sum;
+  complex plp;
+  su3_matrix *tempmat;
 
-    sum = cmplx(0.0,0.0);
-    FORALLSITES(i,st){lattice[i].tempmat2 = lattice[i].link[TUP];}
-    for(t=1;t<nt;t++){
-	tag=start_gather_site( F_OFFSET(tempmat2), sizeof(su3_matrix),
-	    TUP, EVENANDODD, gen_pt[0] );
-	wait_gather(tag);
-        FORALLSITES(i,st){
-	    if( st->t != 0 )continue;
-	    if(t==1){
-		mult_su3_nn( &(st->link[TUP]), (su3_matrix *)gen_pt[0][i],
-		    &(st->staple));
-	    }
-	    else {
-		mult_su3_nn( &(st->staple), (su3_matrix *)gen_pt[0][i],
-		    &(st->tempmat2));
-		lattice[i].staple = lattice[i].tempmat2;
-	    }
-	}
-        FORALLSITES(i,st){
-	    st->tempmat1 = *(su3_matrix *)(gen_pt[0][i]);
-	}
-        FORALLSITES(i,st){
-	    st->tempmat2 = st->tempmat1;
-	}
-	cleanup_gather(tag);
+  tempmat = (su3_matrix *)malloc(sites_on_node*sizeof(su3_matrix));
+  if(tempmat == NULL){
+    printf("ploop: Can't allocate temporary\n");
+    terminate(1);
+  }
+
+  sum = cmplx(0.0,0.0);
+  FORALLSITES(i,st){tempmat[i] = lattice[i].link[TUP];}
+  for(t=1;t<nt;t++){
+    tag=start_gather_field( tempmat, sizeof(su3_matrix),
+			    TUP, EVENANDODD, gen_pt[0] );
+    wait_gather(tag);
+    FORALLSITES(i,st){
+      if( st->t != 0 )continue;
+      if(t==1){
+	mult_su3_nn( &(st->link[TUP]), (su3_matrix *)gen_pt[0][i],
+		     &(st->staple));
+      }
+      else {
+	mult_su3_nn( &(st->staple), (su3_matrix *)gen_pt[0][i],
+		     &(tempmat[i]));
+	lattice[i].staple = tempmat[i];
+      }
     }
     FORALLSITES(i,st){
-	if( st->t != 0 )continue;
-	plp = trace_su3( &(st->staple) );
-	CSUM(sum,plp);
+      st->tempmat1 = *(su3_matrix *)(gen_pt[0][i]);
     }
-    g_complexsum( &sum );
-    plp.real = sum.real /((Real)(nx*ny*nz));
-    plp.imag = sum.imag /((Real)(nx*ny*nz));
-    return(plp);
+    FORALLSITES(i,st){
+     tempmat[i] = st->tempmat1;
+    }
+    cleanup_gather(tag);
+  }
+  FORALLSITES(i,st){
+    if( st->t != 0 )continue;
+    plp = trace_su3( &(st->staple) );
+    CSUM(sum,plp);
+  }
+  g_complexsum( &sum );
+  plp.real = sum.real /((Real)(nx*ny*nz));
+  plp.imag = sum.imag /((Real)(nx*ny*nz));
+  return(plp);
 }
