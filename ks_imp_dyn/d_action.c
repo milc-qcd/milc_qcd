af0769e5d4e714cad2a0af7ffa38684dc55e463b
Carleton DeTar <detar@physics.utah.edu>
2005-07-14 04:35:16 +0000

    Moving directory ks_imp_dyn2 to ks_imp_dyn and upgrading version number

---
+++ ks_imp_dyn/d_action.c
@@ -0,0 +1,117 @@
+/*************** d_action.c ****************************************/
+/* MIMD version 7 */
+
+/* Measure total action, as needed by the hybrid Monte Carlo
+   algorithm.  When this routine is called the conjugate gradient
+   should already have been run on the even sites, so that in the case
+   of two masses, the vectors xxx1 and xxx2 contain (M_adjoint*M)^(-1)
+   * phi1 and phi2 and in the case of one mass the vector xxx contains
+   (M_adjoint*M)^(-1) * phi.
+*/
+
+#include "ks_imp_includes.h"	/* definitions files and prototypes */
+Real ahmat_mag_sq(anti_hermitmat *pt);
+
+/*DEBUG*/
+double old_g, old_h, old_f, old_a;
+/*ENDDEBUG*/
+
+double d_action(){
+double hmom_action(),fermion_action();
+    double ssplaq,stplaq,g_action,h_action,f_action;
+
+    d_plaquette(&ssplaq,&stplaq);
+    ssplaq *= -1.0; stplaq *= -1.0;
+    g_action = -beta*volume*(ssplaq+stplaq);
+    node0_printf("PLAQUETTE ACTION: %e\n",g_action);
+
+    rephase(OFF);
+    g_action = (beta/3.0)*imp_gauge_action();
+    rephase(ON);
+    h_action = hmom_action();
+    f_action = fermion_action();
+
+    node0_printf("ACTION: g,h,f = %e  %e  %e  %e\n",
+    g_action, h_action, f_action, g_action+h_action+f_action );
+
+/*DEBUG*/
+node0_printf("DG = %e, DH = %e, DF = %e, D = %e\n",
+g_action-old_g, h_action-old_h, f_action-old_f,
+g_action+h_action+f_action-old_a);
+old_g=g_action; old_h=h_action; old_f=f_action;
+old_a=g_action+h_action+f_action;
+/*ENDDEBUG*/
+
+    return(g_action+h_action+f_action);
+}
+
+/* fermion contribution to the action */
+double fermion_action() {
+register int i;
+register site *s;
+register complex cc;
+double sum;
+    sum=0.0;
+    FOREVENSITES(i,s){
+	/* phi is defined on even sites only */
+#ifdef ONEMASS
+        cc = su3_dot( &(s->phi), &(s->xxx) );
+        sum += (double)cc.real;
+#else
+        cc = su3_dot( &(s->phi1), &(s->xxx1) );
+        sum += (double)cc.real;
+        cc = su3_dot( &(s->phi2), &(s->xxx2) );
+        sum += (double)cc.real;
+#endif
+    }
+    g_doublesum( &sum );
+    return(sum);
+}
+
+/* gauge momentum contribution to the action */
+double hmom_action() {
+register int i,dir;
+register site *s;
+double sum;
+
+    sum=0.0;
+    FORALLSITES(i,s){
+	for(dir=XUP;dir<=TUP;dir++){
+            sum += (double)ahmat_mag_sq( &(s->mom[dir]) ) - 4.0;
+	    /* subtract 1/2 per d.o.f. to help numerical acc. in sum */
+	}
+    }
+    g_doublesum( &sum );
+    return(sum);
+}
+
+/* magnitude squared of an antihermition matrix */
+Real ahmat_mag_sq(anti_hermitmat *pt){
+register Real x,sum;
+    x = pt->m00im; sum  = 0.5*x*x;
+    x = pt->m11im; sum += 0.5*x*x;
+    x = pt->m22im; sum += 0.5*x*x;
+    x = pt->m01.real; sum += x*x;
+    x = pt->m01.imag; sum += x*x;
+    x = pt->m02.real; sum += x*x;
+    x = pt->m02.imag; sum += x*x;
+    x = pt->m12.real; sum += x*x;
+    x = pt->m12.imag; sum += x*x;
+    return(sum);
+}
+
+/* copy a gauge field - an array of four su3_matrices */
+void gauge_field_copy(field_offset src,field_offset dest){
+register int i,dir,src2,dest2;
+register site *s;
+    FORALLSITES(i,s){
+	src2=src; dest2=dest;
+        for(dir=XUP;dir<=TUP; dir++){
+	    su3mat_copy( (su3_matrix *)F_PT(s,src2),
+		(su3_matrix *)F_PT(s,dest2) );
+	    src2 += sizeof(su3_matrix);
+	    dest2 += sizeof(su3_matrix);
+	}
+    }
+    valid_longlinks=valid_fatlinks=0;
+}
---
+++ ks_imp_dyn/update.c
@@ -0,0 +1,136 @@
+/********** update.c ****************************************************/
+/* MIMD version 7 */
+
+/*
+ Update lattice.
+ Improved method for 1-4 flavors:
+	update U by (epsilon/2)*(1-Nf/4)
+	compute PHI
+	update U to epsilon/2
+	compute X
+	update H, full step
+	update U to next time needed
+
+ This routine does not refresh the antihermitian momenta.
+ This routine begins at "integral" time, with H and U evaluated
+ at same time.
+*/
+#include "ks_imp_includes.h"	/* definitions files and prototypes */
+
+int update()  {
+int step, iters=0;
+Real final_rsq;
+#ifdef HMC_ALGORITHM
+double startaction,endaction,d_action();
+Real xrandom;
+#endif
+
+    /* refresh the momenta */
+    ranmom();
+
+    /* do "steps" microcanonical steps"  */
+    for(step=1; step <= steps; step++){
+ 
+#ifdef PHI_ALGORITHM
+        /* generate a pseudofermion configuration only at start*/
+	/* also clear xxx, since zero is our best guess for the solution
+	   with a new random phi field. */
+     	if(step==1){
+	    clear_latvec( F_OFFSET(xxx1), EVENANDODD );
+	    grsource_imp( F_OFFSET(phi1), mass1, EVEN);
+	    clear_latvec( F_OFFSET(xxx2), EVENANDODD );
+	    grsource_imp( F_OFFSET(phi2), mass2, EVEN);
+	}
+
+#ifdef HMC_ALGORITHM
+        /* find action */
+        /* do conjugate gradient to get (Madj M)inverse * phi */
+        if(step==1){
+            /* do conjugate gradient to get (Madj M)inverse * phi */
+	    iters += ks_congrad( F_OFFSET(phi1), F_OFFSET(xxx1), mass1,
+		 niter, rsqmin, EVEN, &final_rsq );
+	    iters += ks_congrad( F_OFFSET(phi2), F_OFFSET(xxx2), mass2,
+		niter, rsqmin, EVEN, &final_rsq );
+
+     	    startaction=d_action();
+            /* copy link field to old_link */
+	    gauge_field_copy( F_OFFSET(link[0]), F_OFFSET(old_link[0]));
+        }
+#endif
+
+	/* update U's to middle of interval */
+     	update_u(0.5*epsilon);
+
+#else /* "R" algorithm */
+       	/* first update the U's to special time interval */
+        /* and generate a pseudofermion configuration */
+	/* probably makes most sense if nflavors1 >= nflavors2 */
+
+       	update_u(epsilon*(0.5-nflavors1/8.0));
+	clear_latvec( F_OFFSET(xxx1), EVENANDODD );
+     	grsource_imp( F_OFFSET(phi1), mass1, EVEN);
+
+       	update_u(epsilon*((nflavors1-nflavors2)/8.0));
+	clear_latvec( F_OFFSET(xxx2), EVENANDODD );
+     	grsource_imp( F_OFFSET(phi2), mass2, EVEN);
+
+	/* update U's to middle of interval */
+     	update_u(epsilon*nflavors2/8.0);
+#endif
+
+        /* do conjugate gradient to get (Madj M)inverse * phi */
+#if 0
+     	iters += ks_congrad( F_OFFSET(phi1), F_OFFSET(xxx1), mass1,
+	    niter, rsqmin, EVEN, &final_rsq );
+     	iters += ks_congrad( F_OFFSET(phi2), F_OFFSET(xxx2), mass2,
+	    niter, rsqmin, EVEN, &final_rsq );
+#else
+	iters += ks_congrad_two_src( F_OFFSET(phi1), F_OFFSET(phi2),
+				     F_OFFSET(xxx1), F_OFFSET(xxx2),
+				     mass1, mass2, niter, rsqmin, 
+				     EVEN, &final_rsq);
+#endif
+	dslash_site( F_OFFSET(xxx1), F_OFFSET(xxx1), ODD );
+	dslash_site( F_OFFSET(xxx2), F_OFFSET(xxx2), ODD );
+	/* now update H by full time interval */
+    	update_h(epsilon);
+
+    	/* update U's by half time step to get to even time */
+    	update_u(epsilon*0.5);
+
+        /* reunitarize the gauge field */
+	rephase( OFF );
+        reunitarize();
+	rephase( ON );
+
+    }	/* end loop over microcanonical steps */
+
+#ifdef HMC_ALGORITHM
+    /* find action */
+    /* do conjugate gradient to get (Madj M)inverse * phi */
+    iters += ks_congrad( F_OFFSET(phi1), F_OFFSET(xxx1), mass1,
+	 niter, rsqmin, EVEN, &final_rsq );
+    iters += ks_congrad( F_OFFSET(phi2), F_OFFSET(xxx2), mass2,
+	 niter, rsqmin, EVEN, &final_rsq );
+    endaction=d_action();
+    /* decide whether to accept, if not, copy old link field back */
+    /* careful - must generate only one random number for whole lattice */
+    if(this_node==0)xrandom = myrand(&node_prn);
+    broadcast_float(&xrandom);
+    if( exp( (double)(startaction-endaction) ) < xrandom ){
+	if(steps > 0)
+	    gauge_field_copy( F_OFFSET(old_link[0]), F_OFFSET(link[0]) );
+#ifdef FN
+	valid_longlinks=valid_fatlinks=0;
+#endif
+	node0_printf("REJECT: delta S = %e\n", (double)(endaction-startaction));
+    }
+    else {
+	node0_printf("ACCEPT: delta S = %e\n", (double)(endaction-startaction));
+    }
+#endif
+
+    if(steps > 0)return (iters/steps);
+    else return(-99);
+}
+
