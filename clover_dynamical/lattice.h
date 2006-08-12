658a1508f0d9bdc7e75c78ecbbeb0973bfb73804
Carleton DeTar <detar@physics.utah.edu>
2006-08-12 16:34:41 +0000

    Port to version 7 and add error tolerance files

--- clover_dynamical/lattice.h
+++ clover_dynamical/lattice.h
@@ -94,6 +94,8 @@ EXTERN	int warms,trajecs,steps,niter,nrestart,propinterval,nflavors;
 EXTERN	Real rsqmin,rsqprop,beta,kappa,clov_c,u0;
 EXTERN	Real epsilon;
 EXTERN	char startfile[MAXFILENAME],savefile[MAXFILENAME];
+EXTERN  double g_ssplaq, g_stplaq;
+EXTERN  double_complex linktrsum;
 EXTERN  char stringLFN[MAXFILENAME];  /** ILDG LFN if applicable **/
 EXTERN	int startflag;	/* beginning lattice: CONTINUE, RELOAD, FRESH */
 EXTERN  int fixflag;  /* gauge fix: COULOMB_GAUGE_FIX, NO_GAUGE_FIX */
