a6d3d810dfe012dfacc3d0d73867c2dfb1e81204
Carleton DeTar <detar@physics.utah.edu>
2006-08-25 05:02:10 +0000

    Fix one stray strict initialization

--- generic_ks/dslash_fn2.c
+++ generic_ks/dslash_fn2.c
@@ -455,7 +455,7 @@ void dslash_fn_field_special(su3_vector *src, su3_vector *dest,
 void ddslash_fn_du0_site( field_offset src, field_offset dest, int parity ) {
    register int i;
    register site *s;
-   register int dir,otherparity;
+   register int dir,otherparity=0;
    register su3_matrix *fat4, *long4;
    msg_tag *tag[16];
 
