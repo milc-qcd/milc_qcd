ee02c7a6c456f846af52fabc08af8fedd1277f98
Carleton DeTar <detar@physics.utah.edu>
2006-12-12 17:04:57 +0000

    Convert record XML to new USQCD metadata specification

--- ks_imp_rhmc/gauge_info.c
+++ ks_imp_rhmc/gauge_info.c
@@ -196,14 +196,25 @@ char *create_QCDML(){
   size_t bytes = 0;
   char *info = (char *)malloc(INFOSTRING_MAX);
   size_t max = INFOSTRING_MAX;
-  char begin[] = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><info>";
-  char end[] = "</info>";
+  char begin[] = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><usqcdInfo><version>1.0</version>";
+  char begin_info[] = "<info>";
+  char end_info[] = "</info>";
+  char end[] = "</usqcdInfo>";
   Real myssplaq = g_ssplaq;  /* Precision conversion */
   Real mystplaq = g_stplaq;  /* Precision conversion */
   Real nersc_linktr = linktrsum.real/3.;  /* Convention and precision */
 
 
   snprintf(info+bytes, max-bytes,"%s",begin);
+  bytes = strlen(info);
+
+  snprintf(info+bytes, max-bytes,"<plaq>%e</plaq>",(myssplaq+mystplaq)/6.);
+  bytes = strlen(info);
+
+  snprintf(info+bytes, max-bytes,"<linktr>%e</linktr>",nersc_linktr);
+  bytes = strlen(info);
+
+  snprintf(info+bytes, max-bytes,"%s",begin_info);
   bytes = strlen(info);
 
   sprint_gauge_info_item(info+bytes, max-bytes,"action.description","%s",
@@ -261,6 +272,10 @@ char *create_QCDML(){
 			 (char *)&mass2,0,0);
   bytes = strlen(info);
 #endif  
+
+  snprintf(info+bytes, max-bytes,"%s",end_info);
+  bytes = strlen(info);
+
   snprintf(info+bytes, max-bytes,"%s",end);
   return info;
 }
