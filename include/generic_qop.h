9bde62d0c897b386e548465f31c734884ab31647
Carleton DeTar <detar@physics.utah.edu>
2007-05-21 14:20:45 +0000

    Distinguish QOPQDP and MILC/QCDOC raw formats.

--- include/generic_qop.h
+++ include/generic_qop.h
@@ -5,7 +5,6 @@
 */
 
 #include <qop.h>
-
 #include "../include/su3.h"
 
 /* milc_to_qop_utilities.c */
@@ -13,7 +12,7 @@ QOP_evenodd_t milc2qop_parity(int milc_parity);
 int qop2milc_parity(QOP_evenodd_t qop_parity);
 QOP_status_t initialize_qop();
 
-/* map_milc_to_qop.c */
+/* map_milc_to_qop*.c */
 
 /* Generic mapping */
 
@@ -22,176 +21,175 @@ QOP_status_t initialize_qop();
 #define create_raw4_G  create_raw4_F_G
 #define create_raw4_F  create_raw4_F_F
 #define create_raw_V   create_raw_F_V
+#define create_raw_D   create_raw_F_D
 
 #define destroy_raw4_G  destroy_raw4_F_G
 #define destroy_raw4_F  destroy_raw4_F_F
 #define destroy_raw_V   destroy_raw_F_V
+#define destroy_raw_D   destroy_raw_F_D
 
 #define create_raw4_G_from_site create_raw4_F_G_from_site
 #define create_raw4_F_from_site create_raw4_F_F_from_site
 #define create_raw_V_from_site  create_raw_F_V_from_site
+#define create_raw_D_from_site  create_raw_F_D_from_site
 
 #define create_raw4_G_from_field create_raw4_F_G_from_field
 #define create_raw4_F_from_field create_raw4_F_F_from_field
 #define create_raw_V_from_field  create_raw_F_V_from_field
+#define create_raw_D_from_field  create_raw_F_D_from_field
+
+#define create_F_from_site4  create_F_F_from_site4
+#define create_G_from_site4  create_F_G_from_site4
+#define create_V_from_site   create_F_V_from_site
+#define create_D_from_site   create_F_D_from_site
+
+#define create_F_from_field4  create_F_F_from_field4
+#define create_G_from_field4  create_F_G_from_field4
+#define create_V_from_field   create_F_V_from_field
+#define create_D_from_field   create_F_D_from_field
 
 #define unload_raw4_G_to_site unload_raw4_F_G_to_site
 #define unload_raw4_F_to_site unload_raw4_F_F_to_site
 #define unload_raw_V_to_site  unload_raw_F_V_to_site
+#define unload_raw_D_to_site  unload_raw_F_D_to_site
 
 #define unload_raw4_G_to_field unload_raw4_F_G_to_field
 #define unload_raw4_F_to_field unload_raw4_F_F_to_field
 #define unload_raw_V_to_field  unload_raw_F_V_to_field
+#define unload_raw_D_to_field  unload_raw_F_D_to_field
 
-#define load_links_and_mom_site load_F_links_and_mom_site
-#define load_links_and_mom_field load_F_links_and_mom_field
+#define unload_F_to_site4  unload_F_F_to_site4
+#define unload_G_to_site4  unload_F_G_to_site4
 
-#define unload_links_and_mom_site unload_F_links_and_mom_site
-#define unload_links_and_mom_field unload_F_links_and_mom_field
+#define unload_F_to_field4  unload_F_F_to_field4
+#define unload_G_to_field4  unload_F_G_to_field4
 
-#define load_V_from_site  load_F_V_from_site
-#define load_V_from_field load_F_V_from_field
 #define unload_V_to_site  unload_F_V_to_site
 #define unload_V_to_field unload_F_V_to_field
+#define unload_D_to_site  unload_F_D_to_site
+#define unload_D_to_field unload_F_D_to_field
 
 #else
 
 #define create_raw4_G  create_raw4_D_G
 #define create_raw4_F  create_raw4_D_F
 #define create_raw_V   create_raw_D_V
+#define create_raw_D   create_raw_D_D
 
 #define destroy_raw4_G  destroy_raw4_D_G
 #define destroy_raw4_F  destroy_raw4_D_F
 #define destroy_raw_V   destroy_raw_D_V
+#define destroy_raw_D   destroy_raw_D_D
 
 #define create_raw4_G_from_site create_raw4_D_G_from_site
 #define create_raw4_F_from_site create_raw4_D_F_from_site
 #define create_raw_V_from_site  create_raw_D_V_from_site
+#define create_raw_D_from_site  create_raw_D_D_from_site
 
 #define create_raw4_G_from_field create_raw4_D_G_from_field
 #define create_raw4_F_from_field create_raw4_D_F_from_field
 #define create_raw_V_from_field  create_raw_D_V_from_field
+#define create_raw_D_from_field  create_raw_D_D_from_field
+
+#define create_F_from_site4  create_D_F_from_site4
+#define create_G_from_site4  create_D_G_from_site4
+#define create_V_from_site   create_D_V_from_site
+#define create_D_from_site   create_D_D_from_site
+
+#define create_F_from_field4  create_D_F_from_field4
+#define create_G_from_field4  create_D_G_from_field4
+#define create_V_from_field   create_D_V_from_field
+#define create_D_from_field   create_D_D_from_field
 
 #define unload_raw4_G_to_site unload_raw4_D_G_to_site
 #define unload_raw4_F_to_site unload_raw4_D_F_to_site
 #define unload_raw_V_to_site  unload_raw_D_V_to_site
+#define unload_raw_D_to_site  unload_raw_D_D_to_site
 
 #define unload_raw4_G_to_field unload_raw4_D_G_to_field
 #define unload_raw4_F_to_field unload_raw4_D_F_to_field
 #define unload_raw_V_to_field  unload_raw_D_V_to_field
+#define unload_raw_D_to_field  unload_raw_D_D_to_field
 
-#define load_links_and_mom_site load_D_links_and_mom_site
-#define load_links_and_mom_field load_D_links_and_mom_field
+#define unload_F_to_site4  unload_D_F_to_site4
+#define unload_G_to_site4  unload_D_G_to_site4
 
-#define unload_links_and_mom_site unload_D_links_and_mom_site
-#define unload_links_and_mom_field unload_D_links_and_mom_field
+#define unload_F_to_field4  unload_D_F_to_field4
+#define unload_G_to_field4  unload_D_G_to_field4
 
-#define load_V_from_site  load_D_V_from_site
-#define load_V_from_field load_D_V_from_field
 #define unload_V_to_site  unload_D_V_to_site
 #define unload_V_to_field unload_D_V_to_field
+#define unload_D_to_site  unload_D_D_to_site
+#define unload_D_to_field unload_D_D_to_field
 
 #endif
 
-fsu3_matrix ** create_raw4_F_G (void);
-fsu3_matrix ** create_raw4_F_F (void);
-fsu3_vector * create_raw_F_V(void);
+QOP_F3_ColorVector* create_F_V_from_site(field_offset src, int parity);
+QOP_F3_ColorVector* create_F_V_from_field(su3_vector *src, int parity);
+QOP_F3_DiracFermion* create_F_D_from_site(field_offset src,	int parity);
+QOP_F3_DiracFermion* create_F_D_from_field(wilson_vector *src, 
+					       int parity);
+QOP_F3_GaugeField *create_F_G_from_site4(field_offset link, int parity);
+QOP_F3_Force *create_F_F_from_site4(field_offset mom, int parity);
 
-void destroy_raw4_F_G (fsu3_matrix *raw[]);
-void destroy_raw4_F_F (fsu3_matrix *raw[]);
-void destroy_raw_F_V (fsu3_vector *raw);
-
-fsu3_matrix ** create_raw4_F_G_from_site(field_offset src, int milc_parity);
-fsu3_matrix ** create_raw4_F_F_from_site(field_offset src, int milc_parity);
-fsu3_vector * create_raw_F_V_from_site(field_offset src, int milc_parity);
-
-fsu3_matrix ** create_raw4_F_G_from_field(su3_matrix *src, int milc_parity);
-fsu3_matrix ** create_raw4_F_F_from_field(anti_hermitmat *src, 
-					  int milc_parity);
-fsu3_vector * create_raw_F_V_from_field(su3_vector *src, int milc_parity);
-
-void unload_raw4_F_G_to_site(field_offset dest, fsu3_matrix *raw[], 
-			     int milc_parity);
-void unload_raw4_F_F_to_site(field_offset dest, fsu3_matrix *raw[], 
-			     int milc_parity);
-void unload_raw_F_V_to_site(field_offset dest, fsu3_vector *raw, 
-			    int milc_parity);
-
-void unload_raw4_F_G_to_field(su3_matrix *dest, fsu3_matrix *raw[], 
-			      int milc_parity);
-void unload_raw4_F_F_to_field(anti_hermitmat *dest, fsu3_matrix *raw[], 
-			      int milc_parity);
-void unload_raw_F_V_to_field(su3_vector *dest, fsu3_vector *raw, 
-			     int milc_parity);
-
-void load_F_links_and_mom_site(QOP_F3_GaugeField **links, QOP_F3_Force **mom, 
-			       fsu3_matrix ***rawlinks, fsu3_matrix ***rawmom);
-void load_F_links_and_mom_field(QOP_F3_GaugeField **links, QOP_F3_Force **mom,
-				fsu3_matrix ***rawlinks, fsu3_matrix ***rawmom,
-				su3_matrix *srclink, 
-				anti_hermitmat *srcmom);
-
-void unload_F_links_and_mom_site(QOP_F3_GaugeField **links, QOP_F3_Force **mom,
-				 fsu3_matrix ***rawlinks, 
-				 fsu3_matrix ***rawmom);
-void unload_F_links_and_mom_field(su3_matrix *dstlink, anti_hermitmat *dstmom,
-	  QOP_F3_GaugeField **links, QOP_F3_Force **mom, 
-          fsu3_matrix ***rawlinks, fsu3_matrix ***rawmom);
-
-void load_F_V_from_site(QOP_F3_ColorVector** dest, field_offset src, 
-			int parity);
-void load_F_V_from_field(QOP_F3_ColorVector** dest, su3_vector *src, 
-			int parity);
 void unload_F_V_to_site(field_offset dest, QOP_F3_ColorVector* src, 
 			int parity);
 void unload_F_V_to_field(su3_vector *dest, QOP_F3_ColorVector* src, 
 			int parity);
-
-
-dsu3_matrix ** create_raw4_D_G (void);
-void destroy_raw4_D_G (dsu3_matrix *raw[]);
-dsu3_matrix ** create_raw4_D_F (void);
-void destroy_raw4_D_F (dsu3_matrix *raw[]);
-dsu3_vector * create_raw_D_V(void);
-void destroy_raw_D_V (dsu3_vector *raw);
-dsu3_matrix ** create_raw4_D_G_from_site(field_offset src, int milc_parity);
-dsu3_matrix ** create_raw4_D_G_from_field(su3_matrix *src, int milc_parity);
-dsu3_matrix ** create_raw4_D_F_from_site(field_offset src, int milc_parity);
-dsu3_matrix ** create_raw4_D_F_from_field(anti_hermitmat *src, 
-					  int milc_parity);
-dsu3_vector * create_raw_D_V_from_site(field_offset src, int milc_parity);
-dsu3_vector * create_raw_D_V_from_field(su3_vector *src, int milc_parity);
-void unload_raw4_D_G_to_site(field_offset dest, dsu3_matrix *raw[], 
-			     int milc_parity);
-void unload_raw4_D_G_to_field(su3_matrix *dest, dsu3_matrix *raw[], 
-			      int milc_parity);
-void unload_raw4_D_F_to_site(field_offset dest, dsu3_matrix *raw[], 
-			     int milc_parity);
-void unload_raw4_D_F_to_field(anti_hermitmat *dest, dsu3_matrix *raw[], 
-			      int milc_parity);
-void unload_raw_D_V_to_site(field_offset dest, dsu3_vector *raw, 
-			    int milc_parity);
-void unload_raw_D_V_to_field(su3_vector *dest, dsu3_vector *raw, 
-			     int milc_parity);
-
-void load_D_links_and_mom_site(QOP_D3_GaugeField **links, QOP_D3_Force **mom,
-			       dsu3_matrix ***rawlinks, dsu3_matrix ***rawmom);
-void load_D_links_and_mom_field(QOP_D3_GaugeField **links, QOP_D3_Force **mom,
-				dsu3_matrix ***rawlinks, dsu3_matrix ***rawmom,
-				su3_matrix *srclink, anti_hermitmat *srcmom);
-void unload_D_links_and_mom_site(QOP_D3_GaugeField **links, QOP_D3_Force **mom,
-			 dsu3_matrix ***rawlinks, dsu3_matrix ***rawmom);
-void unload_D_links_and_mom_field(su3_matrix *dstlink, anti_hermitmat *dstmom,
-			QOP_D3_GaugeField **links, QOP_D3_Force **mom, 
-			dsu3_matrix ***rawlinks, dsu3_matrix ***rawmom);
-
-void load_D_V_from_site(QOP_D3_ColorVector** dest, field_offset src, 
+void unload_F_D_to_site(field_offset dest, QOP_F3_DiracFermion* src, 
 			int parity);
-void load_D_V_from_field(QOP_D3_ColorVector** dest, su3_vector *src, 
+void unload_F_D_to_field(wilson_vector *dest, QOP_F3_DiracFermion* src, 
 			int parity);
+void unload_F_F_to_site4(field_offset link, QOP_F3_Force *qop, 
+			 int parity);
+void unload_F_G_to_site4(field_offset link, QOP_F3_GaugeField *qop, 
+			 int parity);
+
+QOP_F3_FermionLinksAsqtad* create_F_L_from_site_gauge( QOP_info_t *info, 
+   QOP_asqtad_coeffs_t *coeffs, field_offset src, int parity);
+
+QOP_F3_FermionLinksAsqtad* create_F_L_from_sites( field_offset fat, 
+			 field_offset lng, int parity);
+
+QOP_F3_FermionLinksAsqtad* create_F_L_from_fields( su3_matrix *fat, 
+			  su3_matrix *lng, int parity);
+
+void unload_F_L_to_fields( su3_matrix *fat, su3_matrix *lng, 
+			  QOP_F3_FermionLinksAsqtad* qop, int parity);
+
+
+QOP_D3_ColorVector* create_D_V_from_site(field_offset src, int parity);
+QOP_D3_ColorVector* create_D_V_from_field(su3_vector *src, int parity);
+QOP_D3_DiracFermion* create_D_D_from_site(field_offset src,	int parity);
+QOP_D3_DiracFermion* create_D_D_from_field(wilson_vector *src, 
+					       int parity);
+QOP_D3_GaugeField *create_D_G_from_site4(field_offset link, int parity);
+QOP_D3_Force *create_D_F_from_site4(field_offset mom, int parity);
+
 void unload_D_V_to_site(field_offset dest, QOP_D3_ColorVector* src, 
 			int parity);
 void unload_D_V_to_field(su3_vector *dest, QOP_D3_ColorVector* src, 
 			int parity);
 
-#endif /* GENERIC_QOP_H */
+void unload_D_D_to_site(field_offset dest, QOP_D3_DiracFermion* src, 
+			int parity);
+void unload_D_D_to_field(wilson_vector *dest, QOP_D3_DiracFermion* src, 
+			int parity);
+void unload_D_F_to_site4(field_offset link, QOP_D3_Force *qop, 
+			 int parity);
+void unload_D_G_to_site4(field_offset link, QOP_D3_GaugeField *qop,
+			 int parity);
+
+QOP_D3_FermionLinksAsqtad* create_D_L_from_site_gauge( QOP_info_t *info, 
+   QOP_asqtad_coeffs_t *coeffs, field_offset src, int parity);
+
+QOP_D3_FermionLinksAsqtad* create_D_L_from_sites( field_offset fat, 
+			 field_offset lng, int parity);
+
+QOP_D3_FermionLinksAsqtad* create_D_L_from_fields( su3_matrix *fat, 
+			  su3_matrix *lng, int parity);
+
+void unload_D_L_to_fields( su3_matrix *fat, su3_matrix *lng, 
+			  QOP_D3_FermionLinksAsqtad* qop, int parity);
+
+#endif /* GENERIC_QOPQDP_H */
