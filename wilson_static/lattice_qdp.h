b45d14fe6e11eb14bc3b5269ba97dbc39fd13f24
Carleton DeTar <detar@physics.utah.edu>
2007-05-21 04:35:19 +0000

    Isolate inclusion of qdp.h for faster compilation

---
+++ wilson_static/lattice_qdp.h
@@ -0,0 +1,19 @@
+#ifndef _LATTICE_QDP_H
+#define _LATTICE_QDP_H
+
+#ifdef HAVE_QDP
+
+#include <qdp.h>
+
+#ifdef CONTROL
+#define EXTERN 
+#else
+#define EXTERN extern
+#endif
+
+EXTERN QDP_Shift neighbor3[4];
+EXTERN QDP_Shift shiftdirs[4];
+EXTERN QDP_ShiftDir shiftfwd[4], shiftbck[4];
+
+#endif
+#endif /* _LATTICE_QDP_H */
