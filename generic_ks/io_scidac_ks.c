21ca4d8fb581f1b067abd4a56a7a95e371006705
Carleton DeTar <detar@physics.utah.edu>
2007-10-09 20:09:45 +0000

    Support color vector field I/O (e.g. for eigenvalues)

--- generic_ks/io_scidac_ks.c
+++ generic_ks/io_scidac_ks.c
@@ -2,6 +2,8 @@
 /* MILC/QIO interface for KS fields */
 /* MIMD version 7 */
 /* CD 2/2005
+   CD 6/2007 Converted standard routines to macros and moved to
+   generic/io_scidac.c
 */
 
 #include "generic_ks_includes.h"
@@ -10,213 +12,20 @@
 #include "../include/io_scidac_ks.h"
 #include <string.h>
 
-/* Factory function for moving color vectors from the site structure
-   to single precision output */
-/* arg must point to the field_offset of the field */
-
-void vget_F3_V_from_site(char *buf, size_t index, int count, void *arg)
-{
-  int i,j;
-  /* Assume output vector is single precision */
-  fsu3_vector *dest = (fsu3_vector *)buf;
-  field_offset src = *((field_offset *)arg);
-  site *s = &lattice[index];
-  /* Source can be any precision */
-  su3_vector *src_vec = (su3_vector *)F_PT(s,src);
-
-  /* Copy, changing precision, if necessary */
-  for(j = 0; j < count; j++)
-    for(i = 0; i < 3; i++){
-      dest[j].c[i].real = src_vec[j].c[i].real;
-      dest[j].c[i].imag = src_vec[j].c[i].imag;
-    }
-}
-
-/* Factory function for moving color vectors from the site structure
-   to single precision output */
-/* arg must point to the field_offset of the field */
-
-void vget_F3_V_from_field(char *buf, size_t index, int count, void *arg)
-{
-  int i,j;
-  /* Assume output vector is single precision */
-  fsu3_vector *dest = (fsu3_vector *)buf;
-  /* Source can be any precision */
-  su3_vector *src = (su3_vector *)arg;
-  su3_vector *src_vec = src + index * count;
-
-  /* Copy, changing precision, if necessary */
-  for(j = 0; j < count; j++)
-    for(i = 0; i < 3; i++){
-      dest[j].c[i].real = src_vec[j].c[i].real;
-      dest[j].c[i].imag = src_vec[j].c[i].imag;
-    }
-}
-
-int write_F3_V_from_site(QIO_Writer *outfile, char *xml_write_lattice, 
-		    field_offset src, int count)
-{
-  QIO_String *xml_record_out;
-  int status;
-  QIO_RecordInfo *rec_info;
-  
-  /* We assume output precision is single */
-  char qdptype[] = "QDP_F3_ColorVector";
-  char prec[] = "F";
-  int datum_size = sizeof(fsu3_vector);
-  int word_size = sizeof(float);
-
-  /* Create the record info for the field */
-  rec_info = QIO_create_record_info(QIO_FIELD, qdptype, prec, 3,
-				    0, datum_size, count);
-  /* Create the record XML for the field */
-  xml_record_out = QIO_string_create();
-  QIO_string_set(xml_record_out,xml_write_lattice);
-
-  /* Write the record for the field */
-  status = QIO_write(outfile, rec_info, xml_record_out, vget_F3_V_from_site, 
-		     count*datum_size, word_size, (void *)&src);
-  if(status != QIO_SUCCESS)return 1;
-
-  QIO_destroy_record_info(rec_info);   rec_info = NULL;
-  QIO_string_destroy(xml_record_out);  xml_record_out = NULL;
-
-  return 0;
-}
-
-int write_F3_V_from_field(QIO_Writer *outfile, char *xml_write_lattice, 
-			 su3_vector *src, int count)
-{
-  QIO_String *xml_record_out;
-  int status;
-  QIO_RecordInfo *rec_info;
-  
-  /* We assume output precision is single */
-  char qdptype[] = "QDP_F3_ColorVector";
-  char prec[] = "F";
-  int datum_size = sizeof(fsu3_vector);
-  int word_size = sizeof(float);
-
-  /* Create the record info for the field */
-  rec_info = QIO_create_record_info(QIO_FIELD, qdptype, prec, 3,
-				    0, datum_size, count);
-  /* Create the record XML for the field */
-  xml_record_out = QIO_string_create();
-  QIO_string_set(xml_record_out,xml_write_lattice);
-
-  /* Write the record for the field */
-  status = QIO_write(outfile, rec_info, xml_record_out, vget_F3_V_from_field, 
-		     count*datum_size, word_size, (void *)src);
-  if(status != QIO_SUCCESS)return 1;
-
-  QIO_destroy_record_info(rec_info);    rec_info = NULL;
-  QIO_string_destroy(xml_record_out);   xml_record_out = NULL;
-
-  return 0;
-}
-
-/* Factory function for moving single precision KS vector from input
-   to site structure */
-void vput_F3_V_to_site(char *buf, size_t index, int count, void *arg)
-{
-  int i,j;
-  fsu3_vector *src = (fsu3_vector *)buf;
-  field_offset dest = *((field_offset *)arg);
-  site *s = &lattice[index];
-  /* Destination can be any precision */
-  su3_vector *dest_vec = (su3_vector *)F_PT(s,dest);
-  
-  /* Copy, changing precision, if necessary */
-  for(j = 0; j < count; j++)
-    for(i = 0; i < 3; i++){
-      dest_vec[j].c[i].real = src[j].c[i].real;
-      dest_vec[j].c[i].imag = src[j].c[i].imag;
-    }
-}
-
-/* Factory function for moving single precision KS vector from input
-   to field-major vector */
-void vput_F3_V_to_field(char *buf, size_t index, int count, void *arg)
-{
-  int i,j;
-  fsu3_vector *src = (fsu3_vector *)buf;
-  su3_vector *dest = (su3_vector *)arg;
-  /* Destination can be any precision */
-  su3_vector *dest_vec = dest + index * count;
-  
-  /* Copy, changing precision, if necessary */
-  for(j = 0; j < count; j++)
-    for(i = 0; i < 3; i++){
-      dest_vec[j].c[i].real = src[j].c[i].real;
-      dest_vec[j].c[i].imag = src[j].c[i].imag;
-    }
-}
-
-/* Read a set of color vectors */
-int read_F3_V_to_site(QIO_Reader *infile, field_offset dest, int count)
-{
-  QIO_String *xml_record_in;
-  QIO_RecordInfo rec_info;
-  int status;
-  /* We assume input precision is single */
-  int datum_size = sizeof(fsu3_vector);
-  int word_size = sizeof(float);
-  
-  /* Read the field record */
-  xml_record_in = QIO_string_create();
-  status = QIO_read(infile, &rec_info, xml_record_in, 
-		    vput_F3_V_to_site, count*datum_size, 
-		    word_size, (void *)&dest);
-  node0_printf("Record info \n\"%s\"\n",QIO_string_ptr(xml_record_in));
-  if(status != QIO_SUCCESS)return 1;
-
-  node0_printf("Checksums %x %x\n",
-	       QIO_get_reader_last_checksuma(infile),
-	       QIO_get_reader_last_checksumb(infile));
-
-  QIO_string_destroy(xml_record_in);  xml_record_in = NULL;
-  return 0;
-}
-
-/* Read a set of color vectors */
-int read_F3_V_to_field(QIO_Reader *infile, su3_vector *dest, int count)
-{
-  QIO_String *xml_record_in;
-  QIO_RecordInfo rec_info;
-  int status;
-  int word_size = sizeof(float);
-
-  /* We assume input precision is single */
-  int datum_size = sizeof(fsu3_vector);
-  
-  /* Read the field record */
-  xml_record_in = QIO_string_create();
-  status = QIO_read(infile, &rec_info, xml_record_in, 
-		    vput_F3_V_to_field, count*datum_size, 
-		    word_size, (void *)dest);
-  node0_printf("Record info \n\"%s\"\n",QIO_string_ptr(xml_record_in));
-  if(status != QIO_SUCCESS)return 1;
-
-  node0_printf("Checksums %x %x\n",
-	       QIO_get_reader_last_checksuma(infile),
-	       QIO_get_reader_last_checksumb(infile));
-
-  QIO_string_destroy(xml_record_in);  xml_record_in = NULL;
-  return 0;
-}
-
 /* Write color vectors in SciDAC format, taking data from the site
    structure */
 /* We don't have a MILC format for such a file */
 
-void save_ks_vector_scidac_from_site(char *filename, char *recxml, 
+void save_ks_vector_scidac_from_site(char *filename, char *recinfo, 
 			  int volfmt, int serpar, field_offset src, int count)
 {
   QIO_Layout layout;
   QIO_Filesystem fs;
   QIO_Writer *outfile;
+  QIO_String *filexml;
+  QIO_String *recxml;
   int status;
-  char *filexml;
+  char *fileinfo;
 
   QIO_verbose(QIO_VERB_OFF);
 
@@ -224,20 +33,26 @@ void save_ks_vector_scidac_from_site(char *filename, char *recxml,
   build_qio_layout(&layout);
 
   /* Create the file metadata */
-  filexml = create_ks_XML();
+  fileinfo = create_ks_XML();
 
   /* Define the I/O nodes */
   build_qio_filesystem(&fs);
 
   /* Open file for writing */
+  filexml = QIO_string_create();
+  QIO_string_set(filexml,fileinfo);
   outfile = open_scidac_output(filename, volfmt, serpar, QIO_ILDGNO,
 			       NULL, &layout, &fs, filexml);
   if(outfile == NULL)terminate(1);
+  QIO_string_destroy(filexml);
 
 
   /* Write the lattice field */
+  recxml = QIO_string_create();
+  QIO_string_set(recxml, recinfo);
   status = write_F3_V_from_site(outfile, recxml, src, count);
   if(status)terminate(1);
+  QIO_string_destroy(recxml);
   
   /* Write information */
   if(volfmt == QIO_SINGLEFILE){
@@ -260,17 +75,19 @@ void save_ks_vector_scidac_from_site(char *filename, char *recxml,
   /* Close the file */
   QIO_close_write(outfile);
 
-  free_ks_XML(filexml);
+  free_ks_XML(fileinfo);
 }
 
-void save_ks_vector_scidac_from_field(char *filename, char *recxml, 
+void save_ks_vector_scidac_from_field(char *filename, char *recinfo, 
     int volfmt, int serpar, su3_vector *src, int count)
 {
   QIO_Layout layout;
   QIO_Filesystem fs;
   QIO_Writer *outfile;
+  QIO_String *filexml;
+  QIO_String *recxml;
   int status;
-  char *filexml;
+  char *fileinfo;
 
   QIO_verbose(QIO_VERB_OFF);
 
@@ -278,20 +95,26 @@ void save_ks_vector_scidac_from_field(char *filename, char *recxml,
   build_qio_layout(&layout);
 
   /* Create the file metadata */
-  filexml = create_ks_XML();
+  fileinfo = create_ks_XML();
 
   /* Define the I/O nodes */
   build_qio_filesystem(&fs);
 
   /* Open file for writing */
+  filexml = QIO_string_create();
+  QIO_string_set(filexml, fileinfo);
   outfile = open_scidac_output(filename, volfmt, serpar, QIO_ILDGNO,
 			       NULL, &layout, &fs, filexml);
   if(outfile == NULL)terminate(1);
+  QIO_string_destroy(filexml);
 
 
   /* Write the lattice field */
+  recxml = QIO_string_create();
+  QIO_string_set(recxml, recinfo);
   status = write_F3_V_from_field(outfile, recxml, src, count);
   if(status)terminate(1);
+  QIO_string_destroy(recxml);
   
   /* Write information */
   if(volfmt == QIO_SINGLEFILE){
@@ -314,7 +137,7 @@ void save_ks_vector_scidac_from_field(char *filename, char *recxml,
   /* Close the file */
   QIO_close_write(outfile);
 
-  free_ks_XML(filexml);
+  free_ks_XML(fileinfo);
 }
 
 /* Read color vectors in SciDAC format */
@@ -324,6 +147,7 @@ void restore_ks_vector_scidac_to_site(char *filename, field_offset dest,
   QIO_Layout layout;
   QIO_Filesystem fs;
   QIO_Reader *infile;
+  QIO_String *recxml;
   int status;
 
   QIO_verbose(QIO_VERB_OFF);
@@ -339,8 +163,12 @@ void restore_ks_vector_scidac_to_site(char *filename, field_offset dest,
   if(infile == NULL)terminate(1);
 
   /* Read the lattice field: one color vector */
-  status = read_F3_V_to_site(infile, dest, count);
+  recxml = QIO_string_create();
+  status = read_F3_V_to_site(infile, recxml, dest, count);
   if(status)terminate(1);
+
+  /* Discard for now */
+  QIO_string_destroy(recxml);
 
   /* Close the file */
   QIO_close_read(infile);
@@ -354,6 +182,7 @@ void restore_ks_vector_scidac_to_field(char *filename,
   QIO_Layout layout;
   QIO_Filesystem fs;
   QIO_Reader *infile;
+  QIO_String *recxml;
   int status;
 
   QIO_verbose(QIO_VERB_OFF);
@@ -369,8 +198,12 @@ void restore_ks_vector_scidac_to_field(char *filename,
   if(infile == NULL)terminate(1);
 
   /* Read the lattice field: one color vector */
-  status = read_F3_V_to_field(infile, dest, count);
+  recxml = QIO_string_create();
+  status = read_F3_V_to_field(infile, recxml, dest, count);
   if(status)terminate(1);
+
+  /* Discard for now */
+  QIO_string_destroy(recxml);
 
   /* Close the file */
   QIO_close_read(infile);
