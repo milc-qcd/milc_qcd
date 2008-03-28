782950dd997747bef8321fe9ad93eba06bd87019
Carleton DeTar <detar@physics.utah.edu>
2008-03-28 14:59:57 +0000

    add convert_ksprop_to_wprop_swv m_by_omega_swv and update convert_ksprop_to_wprop

--- generic_wilson/staggered2naive.c
+++ generic_wilson/staggered2naive.c
@@ -31,14 +31,48 @@ static void mult_omega_l(gamma_matrix_t *omega, int x, int y, int z, int t)
   
   /* Definition follows our KS phase conventions */
 
+  /**
   if(z % 2 == 0){ mult_gamma_by_gamma( &gz, omega, &omtmp ); *omega = omtmp; }
   if(y % 2 == 0){ mult_gamma_by_gamma( &gy, omega, &omtmp ); *omega = omtmp; }
   if(x % 2 == 0){ mult_gamma_by_gamma( &gx, omega, &omtmp ); *omega = omtmp; }
   if(t % 2 == 0){ mult_gamma_by_gamma( &gt, omega, &omtmp ); *omega = omtmp; }
+  **/
+  
+  if(t % 2 == 0){ mult_gamma_by_gamma( &gt, omega, &omtmp ); *omega = omtmp; }
+  if(x % 2 == 0){ mult_gamma_by_gamma( &gx, omega, &omtmp ); *omega = omtmp; }
+  if(y % 2 == 0){ mult_gamma_by_gamma( &gy, omega, &omtmp ); *omega = omtmp; }
+  if(z % 2 == 0){ mult_gamma_by_gamma( &gz, omega, &omtmp ); *omega = omtmp; }
   
 }
 
-static void m_by_omega(wilson_propagator *wp, su3_vector *ksp, site *s,
+static void m_by_omega_swv(spin_wilson_vector *swv, su3_vector *ksp, site *s,
+			   int ks_source_r[]){
+
+  int s0;
+  spin_wilson_vector swvtmp;
+  gamma_matrix_t omega;
+
+  /* Initialize the propagator */
+  
+  memset(swv, 0, sizeof(spin_wilson_vector));
+
+  /* Start with WP = I_spin x KS_color where I_spin is a unit matrix
+     in spin */
+
+  for(s0 = 0; s0 < 4; s0++)
+    su3vec_copy(ksp, &swv->d[s0].d[s0]);
+
+
+  /* Construct (omega(sink) omega0^+(source))^* */
+  omega = omega0_dag;
+  mult_omega_l(&omega, s->x, s->y, s->z, s->t);
+  //  gamma_conj( &omega, &omega );
+  
+  mult_sw_by_gamma_mat_l( swv, &swvtmp, &omega); 
+  *swv = swvtmp;
+}
+
+static void m_by_omega(wilson_propagator *wp, su3_matrix *ksp, site *s,
 		int ks_source_r[]){
 
   int s0,c0,c1;
@@ -55,7 +89,7 @@ static void m_by_omega(wilson_propagator *wp, su3_vector *ksp, site *s,
   for(s0 = 0; s0 < 4; s0++)
     for(c0 = 0; c0 < 3; c0++)
       for(c1 = 0; c1 < 3; c1++)
-	wp->c[c0].d[s0].d[s0].c[c1] = ksp[c0].c[c1];
+	wp->c[c0].d[s0].d[s0].c[c1] = ksp[c0].e[c0][c1];
 
 
   /* Construct (omega(sink) omega0^+(source))^* */
@@ -70,7 +104,7 @@ static void m_by_omega(wilson_propagator *wp, su3_vector *ksp, site *s,
 }
 
 /* r is the KS source point */
-void convert_ksprop_to_wprop(wilson_propagator *wp, su3_vector *ksp, int r[])
+void convert_ksprop_to_wprop(wilson_propagator *wp, su3_matrix *ksp, int r[])
 {
   int i;
   site *s;
@@ -85,6 +119,26 @@ void convert_ksprop_to_wprop(wilson_propagator *wp, su3_vector *ksp, int r[])
 
   FORALLSITES(i,s){
     m_by_omega( wp+i, ksp+3*i, s, r);
+  }
+
+}
+/* r is the KS source point */
+void convert_ksprop_to_wprop_swv(spin_wilson_vector *swv, 
+				 su3_vector *ksp, int r[])
+{
+  int i;
+  site *s;
+  gamma_matrix_t omega0;
+
+  init_gamma();
+
+  /* Construct source omega adjoint */
+  omega0 = g1;   /* Unit gamma */
+  mult_omega_l(&omega0, r[0], r[1], r[2], r[3]);
+  gamma_adj(&omega0_dag, &omega0);
+
+  FORALLSITES(i,s){
+    m_by_omega_swv( swv+i, ksp+i, s, r);
   }
 
 }
