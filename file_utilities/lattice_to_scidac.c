e77feaa1d4122bfafa2cdb6e31a203d9902fbdb0
Carleton DeTar <detar@physics.utah.edu>
2008-06-03 15:32:18 +0000

    Remove unwanted stdio remapping from this utility

--- file_utilities/lattice_to_scidac.c
+++ file_utilities/lattice_to_scidac.c
@@ -114,8 +114,6 @@ int main(int argc, char *argv[])
 #ifdef HAVE_QDP
   QDP_initialize(&argc, &argv);
 #endif
-  /* Remap standard I/O */
-  if(remap_stdio_from_args(argc, argv) == 1)terminate(1);
 
   this_node = mynode();
   number_of_nodes = numnodes();
