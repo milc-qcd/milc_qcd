abf7cdd9dda577abc7de1449e10e76d5f499a60c
Carleton DeTar <detar@physics.utah.edu>
2009-04-05 17:39:09 +0000

    Add new arg

--- file_utilities/gauge_info_dummy.c
+++ file_utilities/gauge_info_dummy.c
@@ -35,7 +35,7 @@
 /* This routine writes the ASCII info file.  It is called from one of
    the lattice output routines in io_lat4.c.*/
 
-void write_appl_gauge_info(FILE *fp)
+void write_appl_gauge_info(FILE *fp, gauge_file *gf)
 {
 }
 
