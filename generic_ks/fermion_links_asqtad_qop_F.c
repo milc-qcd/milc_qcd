8a558b5a8fafb324e10eeb36a162a13ddc7fdabf
Carleton DeTar <detar@physics.utah.edu>
2009-05-31 12:33:33 +0000

    Fix hisq goof

--- generic_ks/fermion_links_asqtad_qop_F.c
+++ generic_ks/fermion_links_asqtad_qop_F.c
@@ -1,4 +1,4 @@
-/**************** fermion_links_hisq_qop_F.c ***********************/
+/**************** fermion_links_asqtad_qop_F.c ***********************/
 /* MILC Version 7 */
 
 /* For specifically single precision inversions using QOP routines */
@@ -8,6 +8,6 @@
 #undef QOP_Precision
 #define QOP_Precision 1
 
-#include "fermion_links_hisq_qop_P.c"
+#include "fermion_links_asqtad_qop_P.c"
 
 
