0ab3b79fb8e8ca775fb8890f07a52f1455d29221
Carleton DeTar <detar@physics.utah.edu>
2009-05-31 02:14:59 +0000

    Add ionode capability

--- ext_src/lattice.h
+++ ext_src/lattice.h
@@ -53,6 +53,19 @@ typedef struct {
 EXTERN	int nx,ny,nz,nt;	/* lattice dimensions */
 EXTERN  int niter, nrestart;
 EXTERN  int volume;		/* volume of lattice = nx*ny*nz*nt */
+#ifdef FIX_NODE_GEOM
+EXTERN  int node_geometry[4];  /* Specifies fixed "nsquares" (i.e. 4D
+			    hypercubes) for the compute nodes in each
+			    coordinate direction.  Must be divisors of
+			    the lattice dimensions */
+#ifdef FIX_IONODE_GEOM
+EXTERN int ionode_geometry[4]; /* Specifies fixed "nsquares" for I/O
+			     partitions in each coordinate direction,
+			     one I/O node for each square.  The I/O
+			     node is at the origin of the square.
+			     Must be divisors of the node_geometry. */
+#endif
+#endif
 EXTERN  params param;           /* user input parameters */
 EXTERN  double g_ssplaq, g_stplaq;
 EXTERN  double_complex linktrsum;
