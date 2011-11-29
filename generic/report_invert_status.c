21b011cc39f37589c9d3593acceb83d39735f7a2
Carleton DeTar <detar@physics.utah.edu>
2011-11-29 20:03:54 +0000

    Report status of inversion

---
+++ generic/report_invert_status.c
@@ -0,0 +1,24 @@
+/******************** report_invert_status *********************************/
+/* MIMD version 7 */
+
+#include "generic_includes.h"
+
+/* Report inversion status */
+void report_status(quark_invert_control *qic){
+
+  if(this_node != 0)return;
+  if((qic->resid > 0 && qic->final_rsq > qic->resid * qic->resid ) ||
+     (qic->relresid > 0 && qic->final_relrsq > qic->relresid * qic->relresid ))
+    printf(" NOT converged final_rsq= %.2g (cf %.2g) rel = %.2g (cf %.2g) restarts = %d iters= %d \n",
+	   qic->final_rsq, qic->resid * qic->resid, 
+	   qic->final_relrsq, qic->relresid * qic->relresid, 
+	   qic->final_restart, qic->final_iters );
+#ifdef CG_OK
+  else
+    printf(" OK converged final_rsq= %.2g (cf %.2g) rel = %.2g (cf %.2g) restarts = %d iters= %d\n",
+	   qic->final_rsq, qic->resid * qic->resid, 
+	   qic->final_relrsq, qic->relresid * qic->relresid, 
+	   qic->final_restart, qic->final_iters );
+#endif
+} /* report_status */
+
