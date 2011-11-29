1929dbfe3df15793367ce05c229f05ec498ba0a9
Carleton DeTar <detar@physics.utah.edu>
2011-11-29 20:58:11 +0000

    New operator function.  Support new fermion links scheme.

--- generic_ks/mat_invert.c
+++ generic_ks/mat_invert.c
@@ -11,37 +11,53 @@
 #include "generic_ks_includes.h"
 #include "../include/dslash_ks_redefine.h"
 
-/* dst = M src. No parity selection here */
+/* dst = M src. With parity selection */
 
 void ks_dirac_op( su3_vector *src, su3_vector *dst, Real mass, 
-		  ferm_links_t *fn){
+		  int parity, imp_ferm_links_t *fn){
     register int i;
     register site *s;
 
-    dslash_field( src, dst, EVENANDODD, fn);
-    FORALLSITES(i,s){
+    dslash_field( src, dst, parity, fn);
+    FORSOMEPARITY(i,s,parity){
       scalar_mult_add_su3_vector( dst+i, src+i, +2.0*mass, dst+i);
     }
 }
 
-/* dst = Madj src. No parity selection here */
+/* dst = Madj src. With parity selection */
 
 void ks_dirac_adj_op( su3_vector *src, su3_vector *dst, Real mass,
-		      ferm_links_t *fn){
+		      int parity, imp_ferm_links_t *fn){
     register int i;
     register site *s;
 
-    dslash_field( src, dst, EVENANDODD, fn);
-    FORALLSITES(i,s){
+    dslash_field( src, dst, parity, fn);
+    FORSOMEPARITY(i,s,parity){
       scalar_mult_su3_vector( dst+i, -1.0, dst+i);
       scalar_mult_add_su3_vector( dst+i, src+i, 2.0*mass, dst+i);
     }
 }
 
+/* dst = Madj dst. With parity selection */
+
+void ks_dirac_adj_op_inplace( su3_vector *dst, Real mass,
+			      int parity, imp_ferm_links_t *fn){
+    register int i;
+    register site *s;
+    su3_vector *tvec = create_v_field();
+
+    dslash_field( dst, tvec, parity, fn);
+    FORSOMEPARITY(i,s,parity){
+      scalar_mult_su3_vector( dst+i, 2.0*mass, dst+i);
+      scalar_mult_add_su3_vector( dst+i, tvec+i, -1.0, dst+i);
+    }
+    destroy_v_field(tvec);
+}
+
 /* dst = Madj M src with parity selection */
 
 void ks_dirac_opsq( su3_vector *src, su3_vector *dst, Real mass, int parity,
-		    ferm_links_t *fn){
+		    imp_ferm_links_t *fn){
     register int i;
     register site *s;
     int otherparity = 0;
@@ -50,7 +66,7 @@ void ks_dirac_opsq( su3_vector *src, su3_vector *dst, Real mass, int parity,
 
     tmp = (su3_vector *)malloc(sites_on_node * sizeof(su3_vector));
     if(tmp==NULL){
-      printf("ks_dirac_op(%d): no room for tmp\n",this_node);
+      printf("ks_dirac_opsq(%d): no room for tmp\n",this_node);
       terminate(1);
     }
 
@@ -70,36 +86,11 @@ void ks_dirac_opsq( su3_vector *src, su3_vector *dst, Real mass, int parity,
     free(tmp);
 }
 
-/* Compute M^-1 * phi, answer in dest
-  Uses phi, ttt, resid, xxx, and cg_p as workspace */
-int mat_invert_cg( field_offset src, field_offset dest, field_offset temp,
-		   Real mass, int prec, ferm_links_t *fn ){
-    int cgn;
-    quark_invert_control qic;
-
-    qic.prec       = prec;
-    qic.max        = niter;
-    qic.nrestart   = nrestart;
-    qic.resid      = rsqprop;
-    qic.relresid   = 0;
-    qic.parity     = EVENANDODD;
-
-    //    clear_latvec( dest, EVENANDODD );
-    /* Multiply SOURCE by Madjoint */
-    dslash_site( src, F_OFFSET(ttt), EVENANDODD, fn );
-    scalar_mult_add_latvec( F_OFFSET(ttt), src,
-       -2.0*mass, F_OFFSET(ttt), EVENANDODD);
-    scalar_mult_latvec( F_OFFSET(ttt), -1.0, F_OFFSET(ttt), EVENANDODD );
-    cgn = ks_congrad_site( F_OFFSET(ttt), dest, &qic, mass, fn );
-    return cgn;
-}
-
-
 /* This algorithm solves even and odd sites separately */
 
 int mat_invert_cg_field(su3_vector *src, su3_vector *dst, 
 			 quark_invert_control *qic,
-			 Real mass, ferm_links_t *fn ){
+			 Real mass, imp_ferm_links_t *fn ){
     int cgn;
     su3_vector *tmp;
 
@@ -111,7 +102,9 @@ int mat_invert_cg_field(su3_vector *src, su3_vector *dst,
 
     /* "Precondition" both even and odd sites */
     /* temp <- M_adj * src */
-    ks_dirac_adj_op( src, tmp, mass, fn);
+    /* The following operation is done in the prevailing
+       precision.  The algorithm needs to be fixed! */
+    ks_dirac_adj_op( src, tmp, mass, EVENANDODD, fn);
 
     /* We don't call with EVENANDODD anymore because we are
        transitioning to the QOP/QDP standard */
@@ -122,44 +115,53 @@ int mat_invert_cg_field(su3_vector *src, su3_vector *dst,
 
     /* dst_o <- (M_adj M)^-1 temp_o  (odd sites only) */
     qic->parity = ODD;
-    cgn = ks_congrad_field( tmp, dst, qic, mass, fn );
+    cgn += ks_congrad_field( tmp, dst, qic, mass, fn );
 
     free(tmp);
 
-    // check_invert_field2( dst, tmp, mass, 1e-6, fn);
-    // check_invert_field( dst, src, mass, 1e-6, fn);
+    //    check_invert_field2( dst, tmp, mass, 1e-6, fn);
+    //    check_invert_field( dst, src, mass, 1e-6, fn);
     return cgn;
 }
 
 /* Compute M^-1 * phi, answer in dest
   Uses phi, ttt, resid, xxx, and cg_p as workspace */
-int mat_invert_cg_odd( field_offset src, field_offset dest, field_offset temp,
-		   Real mass, int prec, ferm_links_t *fn ){
+int mat_invert_cg( field_offset src, field_offset dest, field_offset temp,
+		   Real mass, int prec, imp_ferm_links_t *fn ){
     int cgn;
     quark_invert_control qic;
+    int i; site *s;
 
     qic.prec       = prec;
+    qic.min        = 0;
     qic.max        = niter;
     qic.nrestart   = nrestart;
-    qic.resid      = rsqprop;
+    qic.parity     = EVENANDODD;
+    qic.start_flag = 1;
+    qic.nsrc = 1;
+    qic.resid      = sqrt(rsqprop);
     qic.relresid   = 0;
-    qic.parity     = ODD;
 
-    //    clear_latvec( dest, EVENANDODD );
-    /* Multiply SOURCE by Madjoint ODD sites only */
-    dslash_site( src, F_OFFSET(ttt), ODD, fn);
-    scalar_mult_add_latvec( F_OFFSET(ttt), src,
-       -2.0*mass, F_OFFSET(ttt), ODD);
-    scalar_mult_latvec( F_OFFSET(ttt), -1.0, F_OFFSET(ttt), ODD );
-    cgn = ks_congrad_site( F_OFFSET(ttt), dest, &qic, mass, fn);
+    su3_vector *tsrc = create_v_field_from_site_member(src);
+    su3_vector *tdest = create_v_field_from_site_member(dest);
+
+    cgn = mat_invert_cg_field( tsrc, tdest, &qic, mass, fn );
+
+    FORALLSITES(i,s){
+      su3vec_copy(tdest+i, (su3_vector *)F_PT(s,dest));
+    }
+
+    destroy_v_field(tdest);
+    destroy_v_field(tsrc);
+
     return cgn;
 }
-
 
 /* Invert using Leo's UML trick */
 
 /* Our M is     (  2m		D_eo   )
 		( -D_eo^adj	2m )
+
     Note D_oe = -D_eo^adj 
 
    define U =	( 2m		-D_eo )
@@ -177,72 +179,39 @@ int mat_invert_cg_odd( field_offset src, field_offset dest, field_offset temp,
    Use M^-1 phi = L L^-1 M^-1 U^-1 U phi
 		= L  (UML)^-1 U phi
 		= L  -(1/2m)(congrad) U phi
+
+   Note that
+
+      M^-1 = ( 2m A          - A D_eo )
+             ( - B D_oe        2m B   )
+
+where  A = (4m^2+D_eo D_eo^adj)^-1 and B = (4m^2+D_oe^adj D_oe)^-1
+
 */
          
-int mat_invert_uml_old(field_offset src, field_offset dest, field_offset temp,
-		       Real mass, int prec, ferm_links_t *fn )
-{
-    int cgn;
-    Real finalrsq;
-    register int i;
-    register site *s;
-
-    if( src==temp ){
-	printf("BOTCH\n"); exit(0);
-    }
-    /* multiply by U - even sites only */
-    dslash_site( src, F_OFFSET(ttt), EVEN, fn);
-    scalar_mult_add_latvec( F_OFFSET(ttt), src,
-       -2.0*mass, temp, EVEN);
-    scalar_mult_latvec( temp, -1.0, temp, EVEN);
-    /* invert with M_adj M even */
-    cgn = ks_congrad( temp, dest, mass, niter, nrestart, rsqprop,
-		      prec, EVEN, &finalrsq, fn );
-    /* multiply by (1/2m)L, does nothing to even sites */
-    /* fix up odd sites , 1/2m (Dslash_oe*dest_e + phi_odd) */
-    dslash_site( dest, F_OFFSET(ttt), ODD, fn );
-    FORODDSITES(i,s){
-	sub_su3_vector( (su3_vector *)F_PT(s,src), &(s->ttt), 
-	    (su3_vector *)F_PT(s,dest) );
-	scalar_mult_su3_vector( (su3_vector *)F_PT(s,dest), 1.0/(2.0*mass),
-	    (su3_vector *)F_PT(s,dest) );
-    }
-    return cgn;
-}
-
 /* This algorithm solves even sites, reconstructs odd and then polishes
    to compensate for loss of significance in the reconstruction
 */
 
 int mat_invert_uml_field(su3_vector *src, su3_vector *dst, 
 			 quark_invert_control *qic,
-			 Real mass, ferm_links_t *fn ){
+			 Real mass, imp_ferm_links_t *fn ){
     int cgn;
     register int i;
     register site *s;
-    su3_vector *tmp, *ttt;
+    su3_vector *tmp = create_v_field();
+    su3_vector *ttt = create_v_field();
+    int even_iters;
 
-    tmp = (su3_vector *)malloc(sites_on_node * sizeof(su3_vector));
-    if(tmp==NULL){
-      printf("mat_invert_uml_field(%d): no room for tmp\n",this_node);
-      terminate(1);
-    }
-    ttt = (su3_vector *)malloc(sites_on_node * sizeof(su3_vector));
-    if(ttt==NULL){
-      printf("mat_invert_uml_field(%d): no room for ttt\n",this_node);
-      terminate(1);
-    }
     /* "Precondition" both even and odd sites */
     /* temp <- M_adj * src */
-    dslash_field( src, ttt, EVENANDODD, fn);
-    FORALLSITES(i,s){
-      scalar_mult_add_su3_vector( ttt+i, src+i,-2.0*mass, tmp+i);
-      scalar_mult_su3_vector( tmp+i, -1.0, tmp+i);
-    }
 
-    /* dst_e <- (M_adj M)^-1 temp_e  (even sites only) */
+    ks_dirac_adj_op( src, tmp, mass, EVENANDODD, fn );
+
+    /* dst_e <- (M_adj M)^-1 tmp_e  (even sites only) */
     qic->parity     = EVEN;
     cgn = ks_congrad_field( tmp, dst, qic, mass, fn );
+    even_iters = qic->final_iters;
 
     /* reconstruct odd site solution */
     /* dst_o <-  1/2m (Dslash_oe*dst_e + src_o) */
@@ -256,127 +225,59 @@ int mat_invert_uml_field(su3_vector *src, su3_vector *dst,
     /* dst_o <- (M_adj M)^-1 temp_o  (odd sites only) */
     qic->parity = ODD;
     cgn = ks_congrad_field( tmp, dst, qic, mass, fn );
+    qic->final_iters += even_iters;
 
-    // check_invert_field( dst, src, mass, 1e-6, fn);
-    free(tmp);
-    free(ttt);
+    //    check_invert_field( dst, src, mass, 1e-6, fn);
+    destroy_v_field(tmp);
+    destroy_v_field(ttt);
 
     return cgn;
 }
 
 int mat_invert_uml(field_offset src, field_offset dest, field_offset temp,
-		   Real mass, int prec, ferm_links_t *fn ){
+		   Real mass, int prec, imp_ferm_links_t *fn ){
     int cgn;
     register int i;
     register site *s;
     quark_invert_control qic;
+    su3_vector *tsrc = create_v_field_from_site_member(src);
+    su3_vector *tdest = create_v_field_from_site_member(dest);
 
     qic.prec       = prec;
+    qic.min        = 0;
     qic.max        = niter;
     qic.nrestart   = nrestart;
-    qic.resid      = rsqprop;
+    qic.start_flag = 0;
+    qic.nsrc       = 1;
+    qic.resid      = sqrt(rsqprop);
     qic.relresid   = 0;
 
-    if( src==temp ){
-	printf("BOTCH\n"); exit(0);
+    cgn = mat_invert_uml_field(tsrc, tdest, &qic, mass, fn);
+
+    FORALLSITES(i,s){
+      su3vec_copy(tdest+i, (su3_vector *)F_PT(s,dest));
     }
-    /* "Precondition" both even and odd sites */
-    /* temp <- M_adj * src */
-    dslash_site( src, F_OFFSET(ttt), EVENANDODD, fn);
-    scalar_mult_add_latvec( F_OFFSET(ttt), src,
-       -2.0*mass, temp, EVENANDODD);
-    scalar_mult_latvec( temp, -1.0, temp, EVENANDODD);
-
-    /* dest_e <- (M_adj M)^-1 temp_e  (even sites only) */
-    qic.parity     = EVEN;
-    cgn = ks_congrad_site( temp, dest, &qic, mass, fn );
-
-    /* reconstruct odd site solution */
-    /* dest_o <-  1/2m (Dslash_oe*dest_e + src_o) */
-    dslash_site( dest, F_OFFSET(ttt), ODD, fn );
-    FORODDSITES(i,s){
-	sub_su3_vector( (su3_vector *)F_PT(s,src), &(s->ttt), 
-	    (su3_vector *)F_PT(s,dest) );
-	scalar_mult_su3_vector( (su3_vector *)F_PT(s,dest), 1.0/(2.0*mass),
-	    (su3_vector *)F_PT(s,dest) );
-    }
-
-    /* Polish off odd sites to correct for possible roundoff error */
-    /* dest_o <- (M_adj M)^-1 temp_o  (odd sites only) */
-    qic.parity = ODD;
-    cgn = ks_congrad_site( temp, dest, &qic, mass, fn );
 
     // check_invert( dest, src, mass, 1e-6, fn);
 
+    destroy_v_field(tdest);
+    destroy_v_field(tsrc);
     return cgn;
 }
 
 /* FOR TESTING: multiply src by matrix and check against dest */
-void check_invert( field_offset src, field_offset dest, Real mass,
-		   Real tol, ferm_links_t *fn){
-    register int i,k,flag;
-    register site *s;
-    Real r_diff, i_diff;
-    double sum,sum2,dflag,dmaxerr,derr;
-    dslash_site( src, F_OFFSET(cg_p), EVENANDODD, fn);
-    FORALLSITES(i,s){
-	scalar_mult_add_su3_vector( &(s->cg_p), (su3_vector *)F_PT(s,src),
-	    +2.0*mass, &(s->cg_p) );
-    }
-    sum2=sum=0.0;
-    dmaxerr=0;
-    flag = 0;
-    FORALLSITES(i,s){
-	for(k=0;k<3;k++){
-	    r_diff = ((su3_vector *)F_PT(s,dest))->c[k].real
-		    - s->cg_p.c[k].real;
-	    i_diff = ((su3_vector *)F_PT(s,dest))->c[k].imag
-		    - s->cg_p.c[k].imag;
-	    if( fabs(r_diff) > tol || fabs(i_diff) > tol ){
-	      printf("site %d color %d  expected ( %.4e , %.4e ) got ( %.4e , %.4e )\n",
-		     i,k,
-		     ((su3_vector *)F_PT(s,dest))->c[k].real,
-		     ((su3_vector *)F_PT(s,dest))->c[k].imag,
-		     s->cg_p.c[k].real,s->cg_p.c[k].imag);
-	      flag++;
-	    }
-	    derr = r_diff*r_diff + i_diff*i_diff;
-	    if(derr>dmaxerr)dmaxerr=derr;
- 	    sum += derr;
-	}
-	sum2 += magsq_su3vec( (su3_vector *)F_PT(s,dest) );
-    }
-    g_doublesum( &sum );
-    g_doublesum( &sum2 );
-    dflag=flag;
-    g_doublesum( &dflag );
-    g_doublemax( &dmaxerr );
-    if(this_node==0){
-      printf("Inversion checked, frac. error = %e\n",sqrt(sum/sum2));
-      printf("Flagged comparisons = %d\n",(int)dflag);
-      printf("Max err. = %e frac. = %e\n",sqrt(dmaxerr),
-	     sqrt(dmaxerr*volume/sum2));
-      fflush(stdout);
-    }
-}
-
-/* FOR TESTING: multiply src by matrix and check against dest */
 void check_invert_field( su3_vector *src, su3_vector *dest, Real mass,
-			 Real tol, ferm_links_t *fn){
+			 Real tol, imp_ferm_links_t *fn){
     register int i,k,flag;
     register site *s;
     Real r_diff, i_diff;
     double sum,sum2,dflag,dmaxerr,derr;
     su3_vector *tmp;
 
-    tmp = (su3_vector *)malloc(sites_on_node * sizeof(su3_vector));
-    if(tmp==NULL){
-      printf("check_invert_field(%d): no room for tmp\n",this_node);
-      terminate(1);
-    }
+    tmp = create_v_field();
 
     /* Compute tmp = M src */
-    ks_dirac_op( src, tmp, mass, fn);
+    ks_dirac_op( src, tmp, mass, EVENANDODD, fn);
 
     sum2=sum=0.0;
     dmaxerr=0;
@@ -404,18 +305,34 @@ void check_invert_field( su3_vector *src, su3_vector *dest, Real mass,
     g_doublesum( &dflag );
     g_doublemax( &dmaxerr );
     if(this_node==0){
-      printf("Inversion checked, frac. error = %e\n",sqrt(sum/sum2));
+      if(sum2 > 0)
+	printf("Inversion checked, frac. error = %e\n",sqrt(sum/sum2));
       printf("Flagged comparisons = %d\n",(int)dflag);
-      printf("Max err. = %e frac. = %e\n",sqrt(dmaxerr),
-	     sqrt(dmaxerr*volume/sum2));
+      printf("Max err. = %e",sqrt(dmaxerr));
+      if(sum2 > 0)
+	printf(" frac. = %e",sqrt(dmaxerr*volume/sum2));
+      printf("\n");
       fflush(stdout);
     }
-    free(tmp);
+    destroy_v_field(tmp);
+}
+
+/* FOR TESTING: multiply src by matrix and check against dest */
+void check_invert( field_offset src, field_offset dest, Real mass,
+		   Real tol, imp_ferm_links_t *fn){
+
+  su3_vector *tsrc = create_v_field_from_site_member(src);
+  su3_vector *tdest = create_v_field_from_site_member(dest);
+  
+  check_invert_field( tsrc, tdest, mass, tol, fn );
+  
+  destroy_v_field(tdest);
+  destroy_v_field(tsrc);
 }
 
 /* FOR TESTING: multiply src by Madj M and check against dest */
 void check_invert_field2( su3_vector *src, su3_vector *dest, Real mass,
-			  Real tol, ferm_links_t *fn){
+			  Real tol, imp_ferm_links_t *fn){
     register int i,k,flag;
     register site *s;
     Real r_diff, i_diff;
@@ -424,7 +341,7 @@ void check_invert_field2( su3_vector *src, su3_vector *dest, Real mass,
 
     tmp = (su3_vector *)malloc(sites_on_node * sizeof(su3_vector));
     if(tmp==NULL){
-      printf("check_invert_field(%d): no room for tmp\n",this_node);
+      printf("check_invert_field2(%d): no room for tmp\n",this_node);
       terminate(1);
     }
 
