f6a1cbdcb3402e842c0d4036fa71527aa6ef3fe6
Carleton DeTar <detar@physics.utah.edu>
2011-11-29 20:26:47 +0000

    Types to match compiler macro FERM_ACTION

---
+++ generic_ks/imp_actions/imp_action_types.h
@@ -0,0 +1,14 @@
+#ifndef _IMP_ACTION_TYPES_H
+#define _IMP_ACTION_TYPES_H
+
+/* Macros defining names of fermion actions for use with the compiler
+   macro FERM_ACTION.  Do not use 0 here (the preprocessor default for an
+   undefined macro.) */
+
+#define HISQ    1
+#define EO_TYPE 2
+#define FN_TYPE 3
+
+#endif /* _IMP_ACTION_TYPES_H */
+
+
