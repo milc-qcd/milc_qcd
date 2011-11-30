3cfdb44e74f53db60b56443927dd8799fc83d103
Carleton DeTar <detar@physics.utah.edu>
2011-11-30 01:29:03 +0000

    Add bicgilu_cl_qop_generic_*

--- include/generic_clover_qop.h
+++ include/generic_clover_qop.h
@@ -12,10 +12,37 @@ int
 bicgilu_cl_milc2qop_F( wilson_vector *milc_src, wilson_vector *milc_dest, 
 		       quark_invert_control *qic, void *dmp);
 
+int 
+bicgilu_cl_qop_generic_F( QOP_info_t *info,
+			  QOP_F3_FermionLinksWilson *qop_links, 
+			  QOP_invert_arg_t *qop_invert_arg,
+			  QOP_resid_arg_t  ***qop_resid_arg,
+			  float *kappas[], int nkappa[], 
+			  QOP_F3_DiracFermion **qop_sol[], 
+			  QOP_F3_DiracFermion *qop_src[], 
+			  int nsrc,		    
+			  int *final_restart,
+			  Real *final_rsq_ptr,
+			  Real *final_relrsq_ptr );
+  
 /* d_bicgilu_cl_qop_D.c */
 
 int 
 bicgilu_cl_milc2qop_D( wilson_vector *milc_src, wilson_vector *milc_dest, 
 		       quark_invert_control *qic, void *dmp);
+
+int 
+bicgilu_cl_qop_generic_D( QOP_info_t *info,
+			  QOP_D3_FermionLinksWilson *qop_links, 
+			  QOP_invert_arg_t *qop_invert_arg,
+			  QOP_resid_arg_t  ***qop_resid_arg,
+			  double *kappas[], int nkappa[], 
+			  QOP_D3_DiracFermion **qop_sol[], 
+			  QOP_D3_DiracFermion *qop_src[], 
+			  int nsrc,		    
+			  int *final_restart,
+			  Real *final_rsq_ptr,
+			  Real *final_relrsq_ptr );
+  
 
 #endif /* _GENERIC_CLOVER_QOP_H */
