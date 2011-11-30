a80965f0ce9771f7278cdff751b4ef42e7ce18b9
Carleton DeTar <detar@physics.utah.edu>
2011-11-30 01:48:36 +0000

    Add UNITARIZE_ITERATIVE

--- include/umethod.h
+++ include/umethod.h
@@ -9,10 +9,11 @@
 //                     derivative is calculated with finite difference
 //   UNITARIZE_RATIONAL -- Y=V*(V^+*V)^-1/2, ()^-1/2 and its derivative
 //                         are calculated with rational approximation
+//   UNITARIZE_HISQ -- to be decided
 //   UNITARIZE_ANALYTIC -- Y=V*(V^+*V)^-1/2, ()^-1/2 and its derivative
 //                         are calculated analytically
 //   UNITARIZE_STOUT -- stout smearing from Morningstar, Peardon paper
-//   UNITARIZE_HISQ -- to be decided
+//   UNITARIZE_ITERATIVE -- iterate V <- 1/2 (V + V^\dagger^-1)
 #define UNITARIZE_NONE 0
 #define UNITARIZE_APE 1
 #define UNITARIZE_ROOT 2
@@ -20,6 +21,7 @@
 #define UNITARIZE_HISQ 4
 #define UNITARIZE_ANALYTIC 5
 #define UNITARIZE_STOUT 6
+#define UNITARIZE_ITERATIVE 7
 
 // Unitarization group
 // Choices are:
