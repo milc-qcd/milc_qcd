f3b2308cf7c56f345b4970f9a6bf341d97f6ab46
Carleton DeTar <detar@physics.utah.edu>
2011-12-03 03:03:49 +0000

    Support new location for regression tests.

--- seterr.sh
+++ seterr.sh
@@ -15,13 +15,13 @@ PAT2=$2
 target=$3
 tol=$4
 
-outtest=out.test.$target
-outsamp=out.sample.$target
-outerrt=out.errtol.$target
-outtesttmp=$outtest.tmp
-outsamptmp=$outsamp.tmp
+outtest=$target.test-out
+outsamp=$target.sample-out
+outerrt=$target.errtol
+outtesttmp=$target.test-tmp
+outsamptmp=$target.sample-tmp
 
-perl ../headtail.pl $PAT1 $PAT2 < $outtest > $outtesttmp
-perl ../headtail.pl $PAT1 $PAT2 < $outsamp > $outsamptmp
-perl ../seterrfile.pl $outtesttmp $outsamptmp $tol > $outerrt
+perl ../../headtail.pl $PAT1 $PAT2 < $outtest > $outtesttmp
+perl ../../headtail.pl $PAT1 $PAT2 < $outsamp > $outsamptmp
+perl ../../seterrfile.pl $outtesttmp $outsamptmp $tol > $outerrt
 
