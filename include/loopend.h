7dd9a493a58cc521d62cf9423d0b07216d9f3940
Carleton DeTar <detar@physics.utah.edu>
2012-02-16 16:53:14 +0000

    Add END_LOOP support for FORSOMEFIELDPARITY

--- include/loopend.h
+++ include/loopend.h
@@ -8,15 +8,27 @@
 #define LOOPEND   /* We take this as the rule for now  -C.D. */
 
 #ifdef LOOPEND
+
 #undef FORSOMEPARITY
 #define FORSOMEPARITY(i,s,choice) \
 { register int loopend;  \
 loopend= (choice)==EVEN ? even_sites_on_node : sites_on_node ; \
 for( i=((choice)==ODD ? even_sites_on_node : 0 ), s= &(lattice[i]); \
 i<loopend; i++,s++)
+
+#undef FORSOMEFIELDPARITY
+#define FORSOMEFIELDPARITY(i,choice) \
+{ register int loopend;  \
+loopend= (choice)==EVEN ? even_sites_on_node : sites_on_node ; \
+for( i=((choice)==ODD ? even_sites_on_node : 0 ); \
+i<loopend; i++)
+
 #define END_LOOP }
+
 #else
+
 #define END_LOOP 	/* define it to be nothing */
+
 #endif
 
 /* NOTE FORSOMEPARITYDOMAIN is still defined in terms of FORSOMEPARITY
