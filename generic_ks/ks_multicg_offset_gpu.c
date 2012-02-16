0b62a9c1ab575257fc638342841704e8926d40db
Carleton DeTar <detar@physics.utah.edu>
2012-02-16 16:37:15 +0000

    Support new version of multimass inverter interface

--- generic_ks/ks_multicg_offset_gpu.c
+++ generic_ks/ks_multicg_offset_gpu.c
@@ -3,12 +3,21 @@
 
 /* The following headers are supplied with the MILC code */
 #include "generic_ks_includes.h"	/* definitions files and prototypes */
+#include "../include/dslash_ks_redefine.h"
+
+#include <quda.h>
+#include <quda_milc_interface.h>
+#include "../include/generic_quda.h"
 #include "../include/loopend.h"
 
-/* The following headers are supplied with QUDA */
-#include <quda.h>
-#include <milc_interface.h>
 
+static su3_vector *ttt,*cg_p;
+static su3_vector *resid;
+static int first_multicongrad = 1;
+
+#ifdef CGTIME
+static const char *prec_label[2] = {"F", "D"};
+#endif
 
 int ks_multicg_offset_field_gpu(
     su3_vector *src,
@@ -19,18 +28,41 @@ int ks_multicg_offset_field_gpu(
     imp_ferm_links_t *fn
     )
 {
-  int i;
+  int i,j;
+  int parity;
   char myname[] = "ks_multicg_offset_field_gpu";
+
+#ifdef CGTIME
+  double dtimec = -dclock();
+#endif
+  double nflop = 1205 + 15*num_offsets;
+  if(parity==EVENANDODD)nflop *=2;
+
   if(qic[0].relresid != 0.){
     printf("%s: GPU code does not yet support a Fermilab-type relative residual\n", myname);
     terminate(1);
   }
 
-  int quda_parity = 0;
+  /* Initialize structure */
+  for(j = 0; j < num_offsets; j++){
+    qic[j].final_rsq     = 0.;
+    qic[j].final_relrsq  = 0.; /* No relative residual in use here */
+    qic[j].size_r        = 0.;
+    qic[j].size_relr     = 0.;
+    qic[j].final_iters   = 0;
+    qic[j].final_restart = 0;  /* No restarts with this algorithm */
+    qic[j].converged     = 1;
+  }
+
+  if( num_offsets==0 )return(0);
+
+  QudaInvertArgs_t inv_args;
+
+  int quda_parity;
   if(qic[0].parity == EVEN){
-    quda_parity = 0;
+    inv_args.evenodd = QUDA_EVEN_PARITY;
   }else if(qic[0].parity == ODD){
-    quda_parity = 1;
+    inv_args.evenodd = QUDA_ODD_PARITY;
   }else{
     printf("%s: Unrecognised parity\n", myname);
     terminate(2);
@@ -44,55 +76,43 @@ int ks_multicg_offset_field_gpu(
   for(i=0; i<num_offsets; ++i){
     tmp = ksp[i].offset;
     offset[i] = tmp;
-    printf("offset[%d] = %lf\n",i,offset[i]);
-  }
-
-
-  for(i=0; i<num_offsets; ++i){
-    printf("ksp[%d].offset = %lf\n",i,ksp[i].offset);      
   }
 
 
   // The following arrays temporarily hold the final residuals
   double* residual = (double*)malloc(num_offsets*sizeof(double));
-  double* fermilab_residual = (double*)malloc(num_offsets*sizeof(double));
+  double* relative_residual = (double*)malloc(num_offsets*sizeof(double));
 
-  const int maxiter  = qic[0].max*qic[0].nrestart;
+
+  inv_args.max_iter  = qic[0].max*qic[0].nrestart;
+  inv_args.restart_tolerance = 1e-1;
+
+
   int num_iters; // number of iterations taken
   su3_matrix* fatlink = get_fatlinks(fn);
   su3_matrix* longlink = get_lnglinks(fn);
 
-  // Work out the grid dimensions
-  int dim[4] = {nx,ny,nz,nt};
-  int const* ng = nodegeom();
-  int node_geom[4] = {ng[0], ng[1], ng[2], ng[3]};
+  initialize_quda();
 
-  printf("Calling qudaMultishiftInvert\n");
-
-
-  const int quda_precision = 2;
-  qudaMultishiftInvert(quda_parity,
-                   dim,
-		   node_geom,
-                   PRECISION,
-                   quda_precision,
-                   num_offsets,
-                   offset,
-                   qic[0].resid,
-                   qic[0].relresid,
-                   maxiter,
-                   fatlink,
-                   longlink,
-                   src,
+  qudaMultishiftInvert(
+		       PRECISION,
+		       qic[0].prec,
+		       num_offsets,
+		       offset,
+		       inv_args,
+		       qic[0].resid,
+		       qic[0].relresid,
+		       fatlink,
+		       longlink,
+		       (void *)src,
 		       (void **)psim,
-                   residual,
-                   fermilab_residual,
-                   &num_iters);
-
-
+		       residual,
+		       relative_residual,
+		       &num_iters);
+  
   for(i=0; i<num_offsets; ++i){
     qic[i].final_rsq = residual[i]*residual[i];
-    qic[i].final_relrsq = fermilab_residual[i]*fermilab_residual[i];
+    qic[i].final_relrsq = relative_residual[i]*relative_residual[i];
     qic[i].final_iters = num_iters;
 
     // check for convergence
@@ -105,7 +125,17 @@ int ks_multicg_offset_field_gpu(
                   
   free(offset); 
   free(residual);
-  free(fermilab_residual);
+  free(relative_residual);
+
+#ifdef CGTIME
+  dtimec += dclock();
+  if(this_node==0){
+    printf("CONGRAD5: time = %e (multicg_offset_QUDA %s) masses = %d iters = %d mflops = %e\n",
+	   dtimec,prec_label[PRECISION-1],num_offsets,num_iters,
+	   (double)(nflop)*volume*
+	   num_iters/(1.0e6*dtimec*numnodes()));
+    fflush(stdout);}
+#endif
 
   return num_iters;
 }
