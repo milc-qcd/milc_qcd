9d969563fc5f37ab2863215835e5f4534236a38a
Carleton DeTar <detar@physics.utah.edu>
2012-02-16 16:24:36 +0000

    Add GPU link fattening support for asqtad and HISQ

--- generic_ks/d_congrad5_fn_gpu.c
+++ generic_ks/d_congrad5_fn_gpu.c
@@ -5,7 +5,6 @@
 // Note that the restart criterion used by QUDA is different from 
 // the restart criterion used by the MILC code
 
-/* The following headers are supplied with the MILC code */
 #include "generic_ks_includes.h"
 #include "../include/prefetch.h"
 #define FETCH_UP 1
@@ -13,71 +12,116 @@
 #define LOOPEND
 #include "../include/loopend.h"
 #include <string.h>
+#include "../include/generic.h"
+
+#include <quda_milc_interface.h>
+#include "../include/generic_quda.h"
 
 #ifdef CGTIME
 static const char *prec_label[2] = {"F", "D"};
 #endif
 
-/* The following headers are supplied with QUDA */
-#include <milc_interface.h>
-
+// Note that the restart criterion used by QUDA is different from 
+// the restart criterion used by the MILC code
 int ks_congrad_parity_gpu(su3_vector *t_src, su3_vector *t_dest, 
 			  quark_invert_control *qic, Real mass,
 			  imp_ferm_links_t *fn)
 {
 
   char myname[] = "ks_congrad_parity_gpu";
+  QudaInvertArgs_t inv_args;
+  int i;
+  double dtimec = -dclock();
+#ifdef CGTIME
+  double nflop = 1187;
+#endif
 
   if(qic->relresid != 0.){
     printf("%s: GPU code does not yet support a Fermilab-type relative residual\n",myname);
     terminate(1);
   }
-  
-  int quda_parity = 0;
+ 
+  /* Initialize qic */
+  qic->size_r = 0;
+  qic->size_relr = 0;
+  qic->final_iters   = 0;
+  qic->final_restart = 0;
+  qic->converged     = 1;
+  qic->final_rsq = 0.;
+  qic->final_relrsq = 0.;
+
+  /* Compute source norm */
+  double source_norm = 0.0;
+  FORSOMEFIELDPARITY(i,qic->parity){
+    source_norm += (double)magsq_su3vec( &t_src[i] );
+  } END_LOOP
+  g_doublesum( &source_norm );
+#ifdef CG_DEBUG
+  node0_printf("congrad: source_norm = %e\n", (double)source_norm);
+#endif
+
+  /* Provide for trivial solution */
+  if(source_norm == 0.0){
+    /* Zero the solution and return zero iterations */
+    FORSOMEFIELDPARITY(i,qic->parity){
+      memset(t_dest + i, 0, sizeof(su3_vector));
+    } END_LOOP
+
+  dtimec += dclock();
+#ifdef CGTIME
+  if(this_node==0){
+    printf("CONGRAD5: time = %e (fn %s) masses = 1 iters = %d mflops = %e\n",
+	   dtimec, prec_label[PRECISION-1], qic->final_iters, 
+	   (double)(nflop*volume*qic->final_iters/(1.0e6*dtimec*numnodes())) );
+    fflush(stdout);}
+#endif
+
+    return 0;
+  }
+
+  /* Initialize QUDA parameters */
+
+  printf("Calling qudaInvert\n");
+  fflush(stdout);
+
+  initialize_quda();
+ 
   if(qic->parity == EVEN){
-    quda_parity = 0;
+	  inv_args.evenodd = QUDA_EVEN_PARITY;
   }else if(qic->parity == ODD){
-    quda_parity = 1;
+	  inv_args.evenodd = QUDA_ODD_PARITY;
   }else{
     printf("%s: Unrecognised parity\n",myname);
     terminate(2);
   }
 
-  int dim[4] = {nx, ny, nz, nt};
-  const int maxiter = qic->max*qic->nrestart;
-  int num_iters;
+  inv_args.max_iter = qic->max*qic->nrestart;
+  inv_args.restart_tolerance = 1e-3;
 
   su3_matrix* fatlink = get_fatlinks(fn);
   su3_matrix* longlink = get_lnglinks(fn);
+  const int quda_precision = 2;
 
-  int const* ng = nodegeom();
-  int node_geom[4] = {ng[0], ng[1], ng[2], ng[3]};
+  double residual, relative_residual;
+  int num_iters;
 
-  printf("Calling qudaInvert\n");
-  double residual, fermilab_residual;
-
-  const double restart_tolerance = 1e-3;
-
-  qudaInvert(quda_parity, 
-					   dim, 
-						 node_geom,
-					   PRECISION,
-				                 qic->prec, 
-						 mass,
-						 qic->resid,
-						 qic->relresid,
-						 restart_tolerance,
-						 maxiter,
-						 fatlink, 
-						 longlink,
-						 t_src, 
-						 t_dest,
-             &residual,
-			       &fermilab_residual, 
-					   &num_iters);
+  qudaInvert(PRECISION,
+	     quda_precision, 
+	     mass,
+	     inv_args,
+	     qic->resid,
+	     qic->relresid,
+	     fatlink, 
+	     longlink,
+	     t_src, 
+	     t_dest,
+	     &residual,
+	     &relative_residual, 
+	     &num_iters);
+  
 
   qic->final_rsq = residual*residual;
-  qic->final_relrsq = fermilab_residual*fermilab_residual;
+  qic->final_relrsq = relative_residual*relative_residual;
   qic->final_iters = num_iters;
 
   // check for convergence 
@@ -86,10 +130,16 @@ int ks_congrad_parity_gpu(su3_vector *t_src, su3_vector *t_dest,
   // Cumulative residual. Not used in practice 
   qic->size_r = 0.0;
   qic->size_relr = 0.0; 
- 
+
+  dtimec += dclock();
+
+#ifdef CGTIME
+  if(this_node==0){
+    printf("CONGRAD5: time = %e (fn_QUDA %s) masses = 1 iters = %d mflops = %e\n",
+	   dtimec, prec_label[quda_precision-1], qic->final_iters, 
+	   (double)(nflop*volume*qic->final_iters/(1.0e6*dtimec*numnodes())) );
+    fflush(stdout);}
+#endif
+
   return num_iters;
 }
-
-  
-
-
---
+++ generic_ks/fermion_links_hisq_load_gpu.c
@@ -0,0 +1,53 @@
+/**************** fermion_links_hisq_load_gpu.c **********************/
+/* MILC Version 7 */
+
+/* Foley 2012 */
+
+/* Entry points 
+
+   load_hisq_aux_links_gpu
+
+*/
+
+#include "generic_ks_includes.h"
+#include "../include/info.h"
+
+#include <quda_milc_interface.h>
+
+void 
+load_hisq_aux_links_gpu(info_t *info, ks_action_paths_hisq *ap, 
+			hisq_auxiliary_t *aux, su3_matrix *links)
+{
+  char myname = "load_hisq_aux_links_gpu";
+  
+  printf("Calling load_hisq_aux_links\n");
+
+  if(ap == NULL){
+    printf("%s(%d): KS action paths not initialized\n", myname, this_node);
+  }
+
+  // load U links
+  memcpy(aux->U_link, links, 4*sizeof(su3_matrix)*sites_on_node);
+  
+
+  double path_coeff[6];
+  path_coeff[0] = ap->p1.act_path_coeff.one_link;
+  path_coeff[1] = ap->p1.act_path_coeff.naik;
+  path_coeff[2] = ap->p1.act_path_coeff.three_staple;
+  path_coeff[3] = ap->p1.act_path_coeff.five_staple;
+  path_coeff[4] = ap->p1.act_path_coeff.seven_staple;
+  path_coeff[5] = ap->p1.act_path_coeff.lepage;
+
+  QudaFatLinkArgs_t fatlink_args;
+  fatlink_args.su3_source = 1; // Is the incoming field an SU(3) gauge field? 
+			       // If so, run SU(3) optimized QUDA code.
+  fatlink_args.use_pinned_memory = 0; // Use page-locked memory in QUDA?
+
+  // Right now, if aux->V_link == NULL 
+  // the level1 fat link is not copied from the GPU back to the CPU.
+  qudaLoadUnitarizedLink(PRECISION, fatlink_args, path_coeff, aux->U_link, aux->V_link, aux->W_unitlink);
+
+  return;
+}
+
+/* fermion_links_hisq_load_gpu.c */
