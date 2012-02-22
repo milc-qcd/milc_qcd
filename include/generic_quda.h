187c74ad543bfba291b652ee134192c88415b6d7
Carleton DeTar <detar@physics.utah.edu>
2012-02-22 03:53:18 +0000

    QUDA support

---
+++ include/generic_quda.h
@@ -0,0 +1,11 @@
+#ifndef _GENERIC_QUDA_H
+#define _GENERIC_QUDA_H
+/******************** generic_quda.h *********************************
+*  MIMD version 7 	 				            *
+*/
+
+#include <quda_milc_interface.h>
+
+int initialize_quda(void);
+
+#endif /* GENERIC_QUDA_H */
