589bf43c8169b9a7ea4dea77bc7410b9b0c88ffc
Carleton DeTar <detar@physics.utah.edu>
2012-03-13 20:26:40 +0000

    version 7.7.5 now

--- include/config.h
+++ include/config.h
@@ -9,7 +9,7 @@
 /* Code version                                                     */
 /********************************************************************/
 
-#define MILC_CODE_VERSION "7.7.4"
+#define MILC_CODE_VERSION "7.7.5"
 
 /********************************************************************/
 /* Compiler/Processor-dependent macros */
