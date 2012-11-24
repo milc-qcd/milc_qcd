cf6d0ac6b7ed881fb3c7ee6f91211ec772707be3
Carleton DeTar <detar@physics.utah.edu>
2012-11-24 05:08:02 +0000

    Upgrade to quda-0.5.0

--- generic/milc_to_quda_utilities.c
+++ generic/milc_to_quda_utilities.c
@@ -10,15 +10,17 @@ static int is_quda_initialized = 0;
 
 int initialize_quda(void){
 
-  QudaLayout_t layout;
+  QudaInitArgs_t init_args;
+  init_args.verbosity = QUDA_SUMMARIZE; // options are QUDA_SILENT, QUDA_SUMMARIZE, QUDA_VERBOSE, QUDA_DEBUG_VERBOSE
   const int dim[4] = {nx, ny, nz, nt};
   int status = 0;
 
   if(is_quda_initialized)return status;
 
-  layout.latsize = dim;
-  layout.machsize = get_logical_dimensions();
-  qudaInit(layout);
+  init_args.layout.device = 0; 								// only valid for single-gpu build
+  init_args.layout.latsize = dim;
+  init_args.layout.machsize = get_logical_dimensions();
+  qudaInit(init_args);
 
   if(status == 0)
     is_quda_initialized = 1;
