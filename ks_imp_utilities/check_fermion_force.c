164b95987330aa58b5a35ee4b244ec3ea3290e96
Carleton DeTar <detar@physics.utah.edu>
2013-12-26 13:25:55 +0000

    Create random field if not reading.

--- ks_imp_utilities/check_fermion_force.c
+++ ks_imp_utilities/check_fermion_force.c
@@ -12,8 +12,8 @@
 BOMB THE COMPILE
 #endif
 
-void check_fermion_force( char *srcfile, int srcflag, field_offset src, 
-			  char *ansfile, int ansflag, Real mass)
+void check_fermion_force( char srcfile[MAX_MASS][MAXFILENAME], int srcflag,
+			  char *ansfile, int ansflag, int nmass, ks_param *ksp)
 {
   Real diff, maxdiff, norm, maxnorm, reldiff;
   int i, dir;
@@ -30,19 +30,44 @@ void check_fermion_force( char *srcfile, int srcflag, field_offset src,
 #endif
   int ff_prec = PRECISION;  /* Just use prevailing precision for now */
   /* Supports only asqtad at the moment */
-  imp_ferm_links_t *fn = get_fm_links(fn_links)[0];
-  
+  imp_ferm_links_t **fn = get_fm_links(fn_links);
+  Real *residues = (Real *)malloc(nmass*sizeof(Real));;
+  su3_vector **src = (su3_vector **)malloc(nmass*sizeof(su3_vector *));
+  su3_matrix *ansmom = (su3_matrix *)malloc(4*sites_on_node*sizeof(su3_matrix));
 
-  /* Make a random source in xxx if we don't reload it */
-
-  if(srcflag == RELOAD_SERIAL){
-    restore_ks_vector_scidac_to_site (srcfile, QIO_SERIAL, src, 1);
-    fflush(stdout);
+  if(residues == NULL){
+    node0_printf("No room for residues\n");
+    terminate(1);
   }
-  else {
-    /* generate g_rand random; phi = Mdagger g_rand */
-    node0_printf("Generating a random source\n");
-    grsource_imp( src, mass, EVENANDODD, fn);
+
+  /* For testing, we just set them to a constant */
+  for(i = 0; i < nmass; i++)
+    residues[i] = ((Real)nflavors)/4.;
+  
+  if(src == NULL){
+    node0_printf("No room for src\n");
+    terminate(1);
+  }
+  
+  if(ansmom == NULL){
+    node0_printf("No room for ansmom\n");
+    terminate(1);
+  }
+
+  for(i = 0; i < nmass; i++)
+    src[i] = create_v_field();
+
+  /* Make a random source in src if we don't reload it */
+
+  for(i = 0; i < nmass; i++){
+    if(srcflag == RELOAD_SERIAL){
+      restore_ks_vector_scidac_to_field(srcfile[i], QIO_SERIAL, src[i], 1);
+      fflush(stdout);
+    }  else {
+      /* generate g_rand random; phi = Mdagger g_rand */
+      node0_printf("Generating random sources\n");
+      grsource_imp_field( src[i], mass, EVENANDODD, fn[i]);
+    }
   }
       
   node0_printf("Computing the fermion force\n"); fflush(stdout);
@@ -62,38 +87,38 @@ void check_fermion_force( char *srcfile, int srcflag, field_offset src,
     }
   }
 
-  eo_fermion_force_oneterm_site( eps, ((Real)nflavors)/4., src, ff_prec, fn_links );
-  /**eo_fermion_force_twoterms( eps/2, 0.5*((Real)nflavors)/4., 
-     1.5*((Real)nflavors)/4., src, src, ff_prec, &fn_links, &ks_act_paths );**/
+  eo_fermion_force_multi( eps, residues, src, nmass, ff_prec, fn_links );
 
   /* If the answer file is given, read it for comparison */
   if(ansflag == RELOAD_SERIAL){
-    restore_color_matrix_scidac_to_site(ansfile, F_OFFSET(ansmom[0]), 4);
+    restore_color_matrix_scidac_to_field(ansfile, ansmom, 4);
     node0_printf("Checking the answer\n"); fflush(stdout);
   }
 
   /* Unpack the answer and compare if possible */
   maxdiff = 0;
+  maxnorm = 0;
   norm = 0;
   FORALLSITES(i,s){
     FORALLUPDIR(dir){
       uncompress_anti_hermitian( &(s->mom[dir]), &tmat );
       /* If we have loaded an answer file, do the comparison */
       if(ansflag == RELOAD_SERIAL){
-	sub_su3_matrix( &(s->ansmom[dir]), &tmat, &diffmat);
+	sub_su3_matrix( ansmom + 4*i + dir, &tmat, &diffmat);
 	diff = sqrt(realtrace_su3( &diffmat, &diffmat ));
 	norm = sqrt(realtrace_su3( &tmat, &tmat));
+	printf("DIFF %g %g\n",norm,diff);
 	if(diff > tol * norm){
-	  printf("Intolerable difference %e node %d site %d\n",
-		 diff,this_node,i);
-	  dumpmat(&(s->ansmom[dir]));
+	  printf("Intolerable relative difference %e node %d site %d\n",
+		 diff/norm,this_node,i);
+	  dumpmat(ansmom + 4*i + dir);
 	  dumpmat(&tmat);
 	}
 	if(maxdiff < diff)maxdiff = diff;
 	if(maxnorm < norm)maxnorm = norm;
       }
       /* In any case, copy the new result to the answer matrix */
-      s->ansmom[dir] = tmat;
+      ansmom[4*i + dir] = tmat;
     }
   }
 
@@ -109,27 +134,30 @@ void check_fermion_force( char *srcfile, int srcflag, field_offset src,
   }      
 
   /* Save source and answer if requested */
+  if(srcflag == SAVE_SERIAL || srcflag == SAVE_PARTFILE_SCIDAC)
+    for(i = 0; i < nmass; i++){
 #ifdef HAVE_QIO
-  if(srcflag == SAVE_SERIAL)
-    save_ks_vector_scidac_from_site(srcfile, "check fermion force",
-				    "source color vector field", 
-				    QIO_SINGLEFILE, QIO_SERIAL, src, 1);
-  else if(srcflag == SAVE_PARTFILE_SCIDAC)
-    save_ks_vector_scidac_from_site(srcfile, "check fermion force",
-				    "source color vector field",
-                                    QIO_PARTFILE, QIO_SERIAL, src, 1);
-  
+      if(srcflag == SAVE_SERIAL)
+	save_ks_vector_scidac_from_field(srcfile[i], "check fermion force",
+					"source color vector field", 
+					QIO_SINGLEFILE, QIO_SERIAL, src[i], 1, PRECISION);
+      else if(srcflag == SAVE_PARTFILE_SCIDAC)
+	save_ks_vector_scidac_from_field(srcfile[i], "check fermion force",
+					"source color vector field",
+					QIO_PARTFILE, QIO_SERIAL, src[i], 1, PRECISION);
+    }
+
   if(ansflag == SAVE_SERIAL){
     filexml = create_QCDML();
-    save_color_matrix_scidac_from_site(ansfile, filexml, 
-       recxml, QIO_SINGLEFILE,  F_OFFSET(ansmom[0]), 4);
+    save_color_matrix_scidac_from_field(ansfile, filexml, 
+        recxml, QIO_SINGLEFILE, ansmom, 4, PRECISION);
     free_QCDML(filexml);
   }
   else if(ansflag == SAVE_PARTFILE_SCIDAC){
     node0_printf("Saving the momentum matrix\n");
     filexml = create_QCDML();
-    save_color_matrix_scidac_from_site(ansfile, filexml, 
-       recxml, QIO_PARTFILE,  F_OFFSET(ansmom[0]), 4);
+    save_color_matrix_scidac_from_field(ansfile, filexml, 
+       recxml, QIO_PARTFILE, ansmom, 4, PRECISION);
     free_QCDML(filexml);
   }
 #endif
