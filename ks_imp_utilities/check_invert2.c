c857ba64598a31f37fea6a554c1c2a98dbb8bb0f
Carleton DeTar <detar@physics.utah.edu>
2013-12-26 13:27:04 +0000

    Switch from site-based to field-based operation

--- ks_imp_utilities/check_invert2.c
+++ ks_imp_utilities/check_invert2.c
@@ -4,15 +4,15 @@
 
 
 /* FOR TESTING: multiply src by Mdagger M and check against dest */
-void check_invert2( field_offset src, field_offset dest, 
-		    field_offset temp, Real mass,
-		    Real tol, int parity,
+void check_invert2( su3_vector *src, su3_vector *dest, 
+		    Real mass, Real tol, int parity,
 		    imp_ferm_links_t *fn){
   register int i,k,flag;
-  register site *s;
   Real r_diff, i_diff;
   double sum,sum2,dflag,dmaxerr,derr;
   int otherparity = 0;
+  su3_vector *temp = create_v_field();
+  su3_vector *cg_p = create_v_field();
 
   switch(parity){
   case EVEN: otherparity=ODD; break;
@@ -20,36 +20,31 @@ void check_invert2( field_offset src, field_offset dest,
   case EVENANDODD: otherparity=EVENANDODD; break;
   }
 					     
-  dslash_site( src, temp, otherparity, fn);
-  dslash_site( temp, F_OFFSET(cg_p), parity, fn);
+  dslash_field( src, temp, otherparity, fn);
+  dslash_field( temp, cg_p, parity, fn);
 
-  FORSOMEPARITY(i,s,parity){
-    scalar_mult_su3_vector( &(s->cg_p), -1.0, &(s->cg_p));
-    scalar_mult_add_su3_vector( &(s->cg_p), (su3_vector *)F_PT(s,src), 
-				+4.0*mass*mass, &(s->cg_p) );
+  FORSOMEFIELDPARITY(i,parity){
+    scalar_mult_su3_vector( cg_p+i, -1.0, cg_p+i);
+    scalar_mult_add_su3_vector( cg_p+i, src+i, +4.0*mass*mass, cg_p+i );
   }
   sum2=sum=0.0;
   dmaxerr=0;
   flag = 0;
-  FORSOMEPARITY(i,s,parity){
+  FORSOMEFIELDPARITY(i,parity){
     for(flag=0,k=0;k<3;k++){
-      r_diff = ((su3_vector *)F_PT(s,dest))->c[k].real
-	- s->cg_p.c[k].real;
-      i_diff = ((su3_vector *)F_PT(s,dest))->c[k].imag
-	- s->cg_p.c[k].imag;
+      r_diff = dest[i].c[k].real - cg_p[i].c[k].real;
+      i_diff = dest[i].c[k].imag - cg_p[i].c[k].imag;
       if( fabs(r_diff) > tol || fabs(i_diff) > tol ){
 	printf("site %d color %d  expected ( %.4e , %.4e ) got ( %.4e , %.4e )\n",
-	       i,k,
-	       ((su3_vector *)F_PT(s,dest))->c[k].real,
-	       ((su3_vector *)F_PT(s,dest))->c[k].imag,
-	       s->cg_p.c[k].real,s->cg_p.c[k].imag);
+	       i,k, dest[i].c[k].real,dest[i].c[k].imag,
+	       cg_p[i].c[k].real, cg_p[i].c[k].imag);
 	flag++;
       }
       derr = r_diff*r_diff + i_diff*i_diff;
       if(derr>dmaxerr)dmaxerr=derr;
       sum += derr;
     }
-    sum2 += magsq_su3vec( (su3_vector *)F_PT(s,dest) );
+    sum2 += magsq_su3vec( dest+i );
   }
   g_doublesum( &sum );
   g_doublesum( &sum2 );
@@ -63,4 +58,6 @@ void check_invert2( field_offset src, field_offset dest,
 	   sqrt(dmaxerr*volume/sum2));
     fflush(stdout);
   }
+  destroy_v_field(temp);
+  destroy_v_field(cg_p);
 }
