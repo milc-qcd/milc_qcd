394a1709c183d88d935dc219677d9c12d1e95d70
Carleton Detar <detar@ds1.fnal.gov>
2014-11-03 22:53:34 -0600

    Fix placement of include for generic_quda.h

--- ks_imp_rhmc/update_u.c
+++ ks_imp_rhmc/update_u.c
@@ -13,9 +13,9 @@
 
 #include "ks_imp_includes.h"	/* definitions files and prototypes */
 
-#include "../include/generic_quda.h"
-
 #ifdef USE_GF_GPU
+
+#include "../include/generic_quda.h"
 
 /* QUDA version */
 
