20ce96aa1c634c79fccbc904a6c8f99a5de185c4
Carleton DeTar <detar@physics.utah.edu>
2015-05-31 15:46:32 -0600

    Use one-link quasi-conserved current instead of symmetric shift current.

--- generic_ks/f_meas_current.c
+++ generic_ks/f_meas_current.c
@@ -31,14 +31,14 @@ static int *
 get_spin_taste(void){
   
   /* Current spin-taste list */
-  char *spin_taste_list[NMU] = {"GX-G1", "GY-G1", "GZ-G1", "GT-G1"};
+  char *spin_taste_list[NMU] = {"rhoxsbfn", "rhoysbfn", "rhozsbfn", "rhotsbfn"};
   static int spin_taste[NMU];
   int mu;
   
   /* Decode spin-taste label */
   for(mu = 0; mu < NMU; mu++){
-    char dummy[6];
-    strncpy(dummy, spin_taste_list[mu], 6);
+    char dummy[32];
+    strncpy(dummy, spin_taste_list[mu], 32);
     spin_taste[mu] = spin_taste_index(dummy);
   }
   
@@ -75,16 +75,16 @@ close_vector_current_file(QIO_Writer *outfile){
 
 /*Write current record for one random source */
 static int
-write_vector_current_record(QIO_Writer *outfile, int jrand, Real mass, complex *j_mu){
+write_vector_current_record(QIO_Writer *outfile, int jrand, Real mass, Real *j_mu){
   int status;
   QIO_String *recxml = QIO_string_create();
   char recinfo[NRECINFO];
   snprintf(recinfo, NRECINFO, "source index %d mass %g", jrand, mass);
   QIO_string_set(recxml, recinfo);
   if(PRECISION == 1)
-    status = write_F_C_from_field(outfile, recxml, j_mu, NMU);
+    status = write_F_R_from_field(outfile, recxml, j_mu, NMU);
   else
-    status = write_D_C_from_field(outfile, recxml, j_mu, NMU);
+    status = write_D_R_from_field(outfile, recxml, j_mu, NMU);
   QIO_string_destroy(recxml);
   return status;
 }
@@ -110,8 +110,8 @@ f_meas_current( int nrand, quark_invert_control *qic, Real mass,
 
   su3_vector *gr = create_v_field();
   su3_vector *gr_mu = create_v_field();
-  su3_vector *M_inv_gr_mu = create_v_field();
-  complex *j_mu = create_c_array_field(NMU);
+  su3_vector *M_inv_gr = create_v_field();
+  Real *j_mu = create_r_array_field(NMU);
 
   /* Open file for writing */
   QIO_Writer *outfile = open_vector_current_file(filename);
@@ -124,27 +124,31 @@ f_meas_current( int nrand, quark_invert_control *qic, Real mass,
   /* Loop over random sources */
   for(jrand = 0; jrand < nrand; jrand++){
     /* Loop over directions for the current */
-      for(mu = 0; mu < NMU; mu++){
-	
 	/* Make random source, and do inversion */
 #ifndef Z2RSOURCE
-	grsource_plain_field( gr, EVENANDODD );
+    grsource_plain_field( gr, EVENANDODD );
 #else
-	z2rsource_plain_field( gr, EVENANDODD );
+    z2rsource_plain_field( gr, EVENANDODD );
 #endif
-	/* Apply the appropriate spin_taste operator for
-	   a local current */
-	spin_taste_op(spin_taste[mu], r_offset, M_inv_gr_mu, gr);
-	spin_taste_op(spin_taste_index("pion05"), r_offset, gr_mu, M_inv_gr_mu);
+    
+    /* M_inv_gr = M^{-1} gr */
+    mat_invert_uml_field( gr, M_inv_gr, qic, mass, fn );
+    
+    /* Loop over directions for the current */
+    for(mu = 0; mu < NMU; mu++){
 	
-	/* M_inv_gr_mu = M^{-1} gr_mu */
-	
-	mat_invert_uml_field( gr_mu, M_inv_gr_mu, qic, mass, fn );
-	
-	/* J_mu = gr.M_inv_gr_mu */
+      //      clear_v_field(gr);
+      //      gr[node_index(3,1,0,2)].c[0].real = 1.;
+
+      /* Apply the appropriate spin_taste operator for
+	 a nearly conserved current.  */
+      spin_taste_op_fn(fn, spin_taste[mu], r_offset, gr_mu, M_inv_gr);
+      spin_taste_op_fn(fn, spin_taste_index("pion05"), r_offset, gr_mu, gr_mu);
+
+	/* J_mu = imag[gr.M_inv_gr] */
 	FORALLFIELDSITES(i){
-	  complex cc = su3_dot( gr+i, M_inv_gr_mu+i );
-	  j_mu[NMU*i + mu] = cc;
+	  complex cc = su3_dot( gr+i, gr_mu+i );
+	  j_mu[NMU*i + mu] = cc.imag;
 	}
       } /* mu */
 
@@ -153,7 +157,7 @@ f_meas_current( int nrand, quark_invert_control *qic, Real mass,
       FORALLFIELDSITES(i){
 	printf("%d %d %d %d ",lattice[i].x, lattice[i].y, lattice[i].z, lattice[i].t);
 	for(mu = 0; mu < NMU; mu++)
-	  printf("(%g, %g) ",j_mu[NMU*i + mu].real,j_mu[NMU*i + mu].imag);
+	  printf("%g ",j_mu[NMU*i + mu]);
 	printf("\n");
       }
 #endif
@@ -173,10 +177,10 @@ f_meas_current( int nrand, quark_invert_control *qic, Real mass,
   close_vector_current_file(outfile);
   node0_printf("Time to write %d records = %e\n", nrand, wtime);
   
-  destroy_v_field(M_inv_gr_mu); M_inv_gr_mu = NULL;
+  destroy_v_field(M_inv_gr); M_inv_gr = NULL;
   destroy_v_field(gr_mu); gr_mu = NULL;
   destroy_v_field(gr); gr = NULL;
-  destroy_c_array_field(j_mu, NMU);
+  destroy_r_array_field(j_mu, NMU);
 }
 
 static Real *
@@ -223,15 +227,14 @@ f_meas_current_multi( int n_masses, int nrand, quark_invert_control *qic,
 
   su3_vector *gr = create_v_field();
   su3_vector *gr_mu = create_v_field();
-  su3_vector *M_inv_gr_mu[n_masses];
-  complex *j_mu[n_masses];
+  su3_vector *M_inv_gr[n_masses];
+  Real *j_mu = create_r_array_field(NMU);
 
   QIO_Writer *outfile[n_masses];
 
   /* Create vector fields */
   for(j = 0; j < n_masses; j++){
-    M_inv_gr_mu[j] = create_v_field();
-    j_mu[j] = create_c_array_field(NMU);
+    M_inv_gr[j] = create_v_field();
   }
 
   /* Open files for writing */
@@ -243,7 +246,7 @@ f_meas_current_multi( int n_masses, int nrand, quark_invert_control *qic,
     }
   }
 
-    /* Load masses from ks_param */
+  /* Load masses from ks_param */
   for(j = 0; j < n_masses; j++)
     mass[j] = ksp[j].mass;
 
@@ -252,54 +255,58 @@ f_meas_current_multi( int n_masses, int nrand, quark_invert_control *qic,
   for(j = 0; j < n_masses; j++)
     fn_multi[j] = fn[ksp[j].naik_term_epsilon_index];
 
+  double wtime = 0.;
   /* Loop over random sources */
   for(jrand = 0; jrand < nrand; jrand++){
-    /* Loop over directions for the current */
-    for(mu = 0; mu < NMU; mu++){
       
-      /* Make random source, and do inversion */
+    /* Make random source, and do inversion */
 #ifndef Z2RSOURCE
-      grsource_plain_field( gr, EVENANDODD );
+    grsource_plain_field( gr, EVENANDODD );
 #else
-      z2rsource_plain_field( gr, EVENANDODD );
+    z2rsource_plain_field( gr, EVENANDODD );
 #endif
 
-      /* Apply the appropriate spin_taste operator for
-	 a local current.  Borrow M_inv_gr_mu[0] as a temporary vector. */
-      spin_taste_op(spin_taste[mu], r_offset, M_inv_gr_mu[0], gr);
-      spin_taste_op(spin_taste_index("pion05"), r_offset, gr_mu, M_inv_gr_mu[0]);
+    /* M_inv_gr = M^{-1} gr */
+    total_iters += mat_invert_multi( gr, M_inv_gr, ksp, n_masses, qic, fn_multi );
 
-      /* M_inv_gr_mu = M^{-1} gr_mu */
-
-      total_iters += mat_invert_multi( gr_mu, M_inv_gr_mu, ksp, n_masses, qic, fn_multi );
-      
-      /* J_mu = gr.M_inv_gr_mu */
-      for(j = 0; j < n_masses; j++){
-	
-	/* psi-bar-psi on even sites = gr.M_inv_gr */
-	FORALLFIELDSITES(i){
-	  complex cc = su3_dot( gr+i, M_inv_gr_mu[j]+i );
-	  j_mu[j][NMU*i + mu] = cc;
-	}
-      } /* j */
-    } /* mu */
-    
     for(j = 0; j < n_masses; j++){
-      int status = write_vector_current_record(outfile[j], jrand, mass[j], j_mu[j]);
+
+      /* Loop over directions for the current */
+      for(mu = 0; mu < NMU; mu++){
+
+	/* Apply the appropriate spin_taste operator for
+	   a nearly conserved current.  */
+	spin_taste_op_fn(fn_multi[j], spin_taste[mu], r_offset, gr_mu, M_inv_gr[j]);
+	spin_taste_op_fn(fn_multi[j], spin_taste_index("pion05"), r_offset, gr_mu, gr_mu);
+	
+	/* J_mu = imag[gr.M_inv_gr] */
+	FORALLFIELDSITES(i){
+	  complex cc = su3_dot( gr+i, gr_mu+i );
+	  j_mu[NMU*i + mu] = cc.imag;
+	}
+      } /* mu */
+
+      wtime -= dclock();
+      int status = write_vector_current_record(outfile[j], jrand, mass[j], j_mu);
       if(status != QIO_SUCCESS){
 	node0_printf("f_meas_current_multi: Failed to write record to %s\n", filenames[j]);
       } else {
 	node0_printf("f_meas_current_multi: Wrote current density for source %d and mass %g on file %s\n", 
 		     jrand, mass[j], filenames[j]);
       }
-    }
+      wtime += dclock();
+
+    } /* j */
   } /* jrand */
   
   for(j = 0; j < n_masses; j++){
     close_vector_current_file(outfile[j]);
-    destroy_c_array_field(j_mu[j], NMU); j_mu[j] = NULL;
-    destroy_v_field(M_inv_gr_mu[j]); M_inv_gr_mu[j] = NULL;
+    destroy_v_field(M_inv_gr[j]); M_inv_gr[j] = NULL;
   }
+
+  node0_printf("Time to write %d records for %d masses = %e\n", nrand, n_masses, wtime);
+
+  destroy_r_array_field(j_mu, NMU); j_mu = NULL;
   
   destroy_v_field(gr_mu); gr_mu = NULL;
   destroy_v_field(gr); gr = NULL;
