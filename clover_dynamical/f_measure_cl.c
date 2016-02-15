c05e59dd8bbab071d97e242824b571533bfcffad
Carleton DeTar <detar@physics.utah.edu>
2016-02-15 12:18:45 -0700

    Add support for the new random number generator

--- clover_dynamical/f_measure_cl.c
+++ clover_dynamical/f_measure_cl.c
@@ -64,11 +64,9 @@ int iters;
 FORALLSITES(i,s){
         for(k=0;k<4;k++)for(j=0;j<3;j++){
 #ifdef SITERAND
-            s->g_rand.d[k].c[j].real = gaussian_rand_no(&(s->site_prn));
-            s->g_rand.d[k].c[j].imag = gaussian_rand_no(&(s->site_prn));
+            s->g_rand.d[k].c[j] = complex_gaussian_rand_no(&(s->site_prn));
 #else
-            s->g_rand.d[k].c[j].real = gaussian_rand_no(&node_prn);
-            s->g_rand.d[k].c[j].imag = gaussian_rand_no(&node_prn);
+            s->g_rand.d[k].c[j] = complex_gaussian_rand_no(&node_prn);
 #endif
 	}
     }
