f01f49942900e6c9bb67dba915bca2c7f816e313
Carleton DeTar <detar@physics.utah.edu>
2016-08-24 14:03:17 -0600

    Change dummy names in Kalkreuter prototype to make clearer

--- include/imp_ferm_links.h
+++ include/imp_ferm_links.h
@@ -263,10 +263,10 @@ int Rayleigh_min(su3_vector *vec, su3_vector **eigVec, Real Tolerance,
 		 int parity, imp_ferm_links_t *fn);
 int Kalkreuter_Ritz(su3_vector **eigVec, double *eigVal, Real Tolerance, 
 		    Real RelTol, int Nvecs, int MaxIter, 
-		    int Restart, int iters, int init );
+		    int Restart, int Kiters, int init );
 int Kalkreuter_PRIMME(su3_vector **eigVec, double *eigVal, Real Tolerance, 
 		      Real RelTol, int Nvecs, int MaxIter, 
-		      int Restart, int iters, int init );
+		      int Restart, int Kiters, int init );
 void Matrix_Vec_mult(su3_vector *src, su3_vector *res, int parity,
 		     imp_ferm_links_t *fn );
 void cleanup_Matrix();
