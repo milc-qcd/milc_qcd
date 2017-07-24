cb4b6656c0605e6b4893ef8dac7ac71d02dbfc2d
Carleton DeTar <detar@physics.utah.edu>
2017-07-23 18:03:03 -0700

    Further improvements

--- Grid/build.sh
+++ Grid/build.sh
@@ -3,13 +3,22 @@
 ARCH=$1
 PK_CC=$2
 PK_CXX=$3
-GIT_BRANCH=feature/block-cg
+GIT_BRANCH=develop
 
 if [ -z ${PK_CXX} ]
 then
   echo "Usage $0 <scalar|avx512|avx2> <CC> <CXX>"
   exit 1
 fi
+
+case ${ARCH} in
+    scalar|avx512|avx2)
+      ;;
+    *)
+      echo "Unsupported ARCH"
+      echo "Usage $0 <scalar|avx512|avx2> <CC> <CXX>"
+      exit 1
+esac
 
 MAKE="make -j4"
 
@@ -22,16 +31,14 @@ MAKE="make -j4"
 
 if [ ! -d ${SRCDIR} ]
 then
-  echo "Fetching ${GIT_BRANCH} branch of package from github"
+  echo "Fetching ${GIT_BRANCH} branch of Grid package from github"
   git clone https://github.com/paboyle/Grid -b ${GIT_BRANCH}
 fi
 
-if [ ! -f ${SRCDIR}/configure ]
-then
-  pushd ${SRCDIR}
-  autoreconf -vif
-  popd
-fi
+# Fetch Eigen package, set up Make.inc files and create Grid configure
+pushd ${SRCDIR}
+./bootstrap.sh
+popd
 
 # Configure only if not already configured                                          
 mkdir -p ${BUILDDIR}
@@ -70,12 +77,17 @@ then
 
        ${SRCDIR}/configure \
             --prefix=${INSTALLDIR} \
-            --enable-mkl=yes \
             --enable-precision=double \
             --enable-simd=GEN \
             --enable-comms=mpi \
+            --host=x86_64-unknown-linux-gnu \
+	    --with-hdf5=/opt/cray/pe/hdf5/1.10.0/INTEL/15.0 \
+	    --with-lime=${HOME}/scidac/install/qio-cori-omp-knl-icc/lib \
             CXX="${PK_CXX}" \
             CXXFLAGS="-std=c++11 -xMIC-AVX512" \
+
+
+	    #            --enable-mkl=yes \
 
              ;;
     *)
