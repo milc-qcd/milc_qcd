977b87e22f7bb458f4a9e1a07b00541a28ca6b75
Carleton DeTar <detar@physics.utah.edu>
2017-09-01 15:01:41 -0700

    Use terminate instead of exit

--- generic/com_qmp.c
+++ generic/com_qmp.c
@@ -389,9 +389,7 @@ initialize_machine(int *argc, char ***argv)
 
 #ifdef HAVE_GRID
   required = QMP_THREAD_MULTIPLE;
-  printf("com_qmp: setting required thread-safety level to QMP_THREAD_MULTIPLE = %d\n", QMP_THREAD_MULTIPLE);
 #else
-  printf("com_qmp: setting required thread-safety level to QMP_THREAD_SINGLE = %d\n", QMP_THREAD_SINGLE);
   required = QMP_THREAD_SINGLE;
 #endif
   
@@ -400,12 +398,12 @@ initialize_machine(int *argc, char ***argv)
     printf("%s\n", QMP_error_string(i));
     printf("com_qmp: Initialize QMP failed.\n");
     fflush(stdout);
-    exit(i);
+    terminate(1);
   }
   if(provided != required){
   printf("com_qmp: required thread-safety level %d can't be provided %d.\n", required, provided);
     fflush(stdout);
-    exit(i);
+    terminate(1);
   }
 
   /* check if 32 bit int is set correctly */
