milc-qcd/milc_qcd

commit d7ed3b46706702a48106f2fb2ab20a5646b3676a

Cleanup of QUDA gauge force and link update routines to use helper functions introduced in 88e0340750f3007ce04c9251469f4a5aebdb9557

generic/gauge_force_imp_gpu.c
@@ -9,16 +9,17 @@
 
 /**#define GFTIME**/ /* For timing gauge force calculation */
 #include "generic_includes.h"	/* definitions files and prototypes */
-
-#include <quda.h>
-#include <quda_milc_interface.h>
-#include "../include/openmp_defs.h"
-
 #include "../include/generic_quda.h"
 
 // gpu code 
 void imp_gauge_force_gpu(Real eps, field_offset mom_off)
 {
+
+#ifdef GFTIME
+  int nflop = 153004;  /* For Symanzik1 action */
+  double dtime = -dclock();
+#endif
+
   Real **loop_coeff = get_loop_coeff();
   //int max_length = get_max_length();
   //int nreps = get_nreps();
@@ -26,42 +27,22 @@ void imp_gauge_force_gpu(Real eps, field_offset mom_off)
   const int num_loop_types = get_nloop();
   double *quda_loop_coeff = (double*)malloc(num_loop_types * sizeof(double));
   int i;
-#ifdef GFTIME
-  int nflop = 153004;  /* For Symanzik1 action */
-  double dtime = -dclock();
-#endif
-
   const Real eb3 = eps*beta/3.0;
   
   initialize_quda();
 
-  su3_matrix *links = qudaAllocatePinned(sites_on_node*4*sizeof(su3_matrix));
-  anti_hermitmat* momentum = qudaAllocatePinned(sites_on_node*4*sizeof(anti_hermitmat));
-
-  int dir,j;
-  site *st;
+  su3_matrix *links = create_G_from_site_quda();
+  anti_hermitmat* momentum = create_M_quda();
 
   for(i=0; i<num_loop_types; ++i) quda_loop_coeff[i] = loop_coeff[i][0];
 
-  FORALLSITES_OMP(i,st,private(dir)){
-    for(dir=XUP; dir<=TUP; ++dir){
-      links[4*i + dir] = st->link[dir];
-    } // dir
-  } END_LOOP_OMP
-
   qudaGaugeForce(PRECISION,num_loop_types,quda_loop_coeff,eb3,links,momentum);
 
-  FORALLSITES_OMP(i,st,private(dir,j)){
-    for(dir=XUP; dir<=TUP; ++dir){
-      for(j=0; j<10; ++j){
-	((Real*)&(st->mom[dir]))[j] += ((Real*)(momentum + 4*i+dir))[j];
-      }
-    }
-  } END_LOOP_OMP
-
+  copy_to_site_from_M_quda(momentum); // insert back into site
+  
   free(quda_loop_coeff);
-  qudaFreePinned(links);
-  qudaFreePinned(momentum);
+  destroy_G_quda(links);
+  destroy_M_quda(momentum);
 
 #ifdef GFTIME
   dtime+=dclock();

ks_imp_rhmc/update_u.c
@@ -22,10 +22,6 @@
 
 void update_u(Real eps){
 
-  int i,dir;
-  site *s;
-  int j;
-
 #ifdef FN
   invalidate_fermion_links(fn_links);
 #endif
@@ -37,32 +33,15 @@ void update_u(Real eps){
   dtime = -dclock();
 #endif
 
-  anti_hermitmat *momentum = qudaAllocatePinned(sites_on_node*4*sizeof(anti_hermitmat));
-  su3_matrix *gauge = qudaAllocatePinned(sites_on_node*4*sizeof(su3_matrix));
+  su3_matrix *links = create_G_from_site_quda();
+  anti_hermitmat* momentum = create_M_from_site_quda();
 
-  // Populate gauge and momentum fields
-  FORALLSITES_OMP(i,s,private(dir)){
-    for(dir=XUP; dir<=TUP; ++dir) {
-      gauge[4*i + dir] = s->link[dir];
-    } // dir
-    for(dir=XUP; dir<=TUP; ++dir) {
-      momentum[4*i + dir] = s->mom[dir];
-    } // dir
-  } END_LOOP_OMP
+  qudaUpdateU(PRECISION, eps, momentum, links);
 
-  qudaUpdateU(PRECISION, eps, momentum, gauge);
+  copy_to_site_from_G_quda(links); // insert back into site
 
-  // Copy updated gauge field back to site structure
-  FORALLSITES_OMP(i,s,private(dir)){
-    for(dir=XUP; dir<=TUP; ++dir){
-      for(j=0; j<18; ++j){
-	s->link[dir] = gauge[4*i + dir];
-      }
-    }
-  } END_LOOP_OMP
-
-  qudaFreePinned(momentum);
-  qudaFreePinned(gauge);
+  destroy_G_quda(links);
+  destroy_M_quda(momentum);
 
 #ifdef GFTIME
   dtime += dclock();

commit e05bd5971684779afcdf1832556588b73ee657ea

Fix bug in gauge force introduced in prior cleanup in d7ed3b46706702a48106f2fb2ab20a5646b3676a

generic/gauge_force_imp_gpu.c
@@ -27,6 +27,7 @@ void imp_gauge_force_gpu(Real eps, field_offset mom_off)
   const int num_loop_types = get_nloop();
   double *quda_loop_coeff = (double*)malloc(num_loop_types * sizeof(double));
   int i;
+  site *st;
   const Real eb3 = eps*beta/3.0;
   
   initialize_quda();
@@ -38,7 +39,13 @@ void imp_gauge_force_gpu(Real eps, field_offset mom_off)
 
   qudaGaugeForce(PRECISION,num_loop_types,quda_loop_coeff,eb3,links,momentum);
 
-  copy_to_site_from_M_quda(momentum); // insert back into site
+  FORALLSITES_OMP(i,st,){
+    for(int dir=XUP; dir<=TUP; ++dir){
+      for(int j=0; j<10; ++j){
+	((Real*)&(st->mom[dir]))[j] += ((Real*)(momentum + 4*i+dir))[j];
+      }
+    }
+  } END_LOOP_OMP
   
   free(quda_loop_coeff);
   destroy_G_quda(links);