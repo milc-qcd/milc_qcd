5c7b61f6e15ef381d369d209148ee794ceea75cd
Carleton Detar <u0029126@ember1.ipoib.privatearch.arches>
2018-05-02 14:38:51 -0600

    Insert/modify phases before checking eigenvectors

--- ks_eigen/control.c
+++ ks_eigen/control.c
@@ -22,7 +22,6 @@ int main( int argc, char **argv ){
   int total_R_iters ;
   double *resid = NULL;
   double chirality, chir_ev, chir_od ;
-  imp_ferm_links_t **fn;
 
   initialize_machine(&argc,&argv);
   /* Remap standard I/O */
@@ -84,23 +83,37 @@ int main( int argc, char **argv ){
     /* Eigenpair calculation */
     STARTTIME;
     
-    fn = get_fm_links(fn_links);
+    imp_ferm_links_t *fn = get_fm_links(fn_links)[0];
+
+    /* Move KS phases and apply time boundary condition, based on the
+       coordinate origin and time_bc */
+    Real bdry_phase[4] = {0.,0.,0.,param.time_bc};
+    /* Set values in the structure fn */
+    set_boundary_twist_fn(fn, bdry_phase, param.coord_origin);
+    /* Apply the operation */
+    boundary_twist_fn(fn, ON);
+
     /* Calculate eigenpairs on even sites */
     param.eigen_param.parity = EVEN;
     total_R_iters=ks_eigensolve(eigVec, eigVal, &param.eigen_param, 1);
     fflush(stdout);
 
     /* Construct eigenpairs on odd sites */
-    construct_eigen_odd(eigVec, eigVal, &param.eigen_param, fn[0]);
+    construct_eigen_odd(eigVec, eigVal, &param.eigen_param, fn);
     
     /* Calculate and print the residues and norms of the eigenvectors */
     resid = (double *)malloc(param.eigen_param.Nvecs*sizeof(double));
     node0_printf("Even site residuals\n");
-    check_eigres( resid, eigVec, eigVal, param.eigen_param.Nvecs, EVEN, fn[0] );
+    check_eigres( resid, eigVec, eigVal, param.eigen_param.Nvecs, EVEN, fn );
     node0_printf("Odd site residuals\n");
-    check_eigres( resid, eigVec, eigVal, param.eigen_param.Nvecs, ODD, fn[0] );
+    check_eigres( resid, eigVec, eigVal, param.eigen_param.Nvecs, ODD, fn );
     fflush(stdout);
-    
+
+  /* Unapply twisted boundary conditions on the fermion links and
+     restore conventional KS phases and antiperiodic BC, if
+     changed. */
+    boundary_twist_fn(fn, OFF);
+
     /* save eigenvectors if requested */
     int status = save_ks_eigen(param.ks_eigen_saveflag, param.ks_eigen_savefile,
       param.eigen_param.Nvecs, eigVal, eigVec, resid, 1);
