482ebabc15383e1ba8534b0dfbfe95752416c2e3
Carleton DeTar <detar@physics.utah.edu>
2018-08-15 19:05:10 -0400

    Add support for node_geom and ionode_geom lines

--- ks_eigen/lattice.h
+++ ks_eigen/lattice.h
@@ -116,6 +116,19 @@ typedef struct {
 EXTERN	int nx,ny,nz,nt;	/* lattice dimensions */
 EXTERN  int volume;		/* volume of lattice = nx*ny*nz*nt */
 EXTERN	int iseed;		/* random number seed */
+#ifdef FIX_NODE_GEOM
+EXTERN  int node_geometry[4];  /* Specifies fixed "nsquares" (i.e. 4D
+			    hypercubes) for the compute nodes in each
+			    coordinate direction.  Must be divisors of
+			    the lattice dimensions */
+#ifdef FIX_IONODE_GEOM
+EXTERN int ionode_geometry[4]; /* Specifies fixed "nsquares" for I/O
+			     partitions in each coordinate direction,
+			     one I/O node for each square.  The I/O
+			     node is at the origin of the square.
+			     Must be divisors of the node_geometry. */
+#endif
+#endif
 EXTERN	int niter,nrestart,nflavors;
 EXTERN  Real u0;
 EXTERN	Real rsqmin,rsqprop;
