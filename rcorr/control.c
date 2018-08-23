24a8af10d862a6000cd7b8cf08b824268d4761be
Shuhei Yamamoto <shuhei.yamamoto.2011@gmail.com>
2018-08-23 10:55:55 -0700

    add macros so as to make differemt executables corresponding to new definitions in Make_template

--- rcorr/control.c
+++ rcorr/control.c
@@ -9,11 +9,21 @@
 #define CONTROL
 #include "rcorr_includes.h"
 
-int 
+#ifdef TIME_TO_TIME
+#define rcorr rcorr_time
+#define print_result print_result_time
+#endif
+
+#ifdef T2TFRMPT2PT
+#define rcorr rcorr_t2tfrmpt2pt
+#define print_result print_result_time
+#endif
+
+int
 main(int argc, char *argv[])
 {
   int prompt;
-  complex **qin_sloppy, **qin_diff;
+  complex **qin_sloppy, **qin_diff; // q for quantity in this case current density
   Real *q, **qblock, **q2block;
   double starttime, endtime;
   
@@ -42,7 +52,7 @@ main(int argc, char *argv[])
     
     if(prompt == 2)continue;   /* For testing */
   
-    /* Create qin arrays */
+    /* Create qin arrays of current densities with each array associated with a random source*/
     qin_sloppy = (complex **)malloc(sizeof(complex *)*param.nrand_sloppy);
     if(qin_sloppy == NULL){
       node0_printf("main: No room for qin\n");
@@ -61,25 +71,37 @@ main(int argc, char *argv[])
       qin_diff[k] = create_c_array_field(NMU);
     }
 
-    /* Create q array and array of variances of the mean over blocks
-       of random sources */
+    /* Create q array of the means of the current densities and 
+                array of their variances over blocks of random sources */
     qblock = (Real **)malloc(sizeof(Real *)*param.nblock);
     q2block = (Real **)malloc(sizeof(Real *)*param.nblock);
     for(ib = 0; ib < param.nblock; ib++){
+#if defined(TIME_TO_TIME) || defined(T2TFRMPT2PT)
       qblock[ib] = create_r_field();
       q2block[ib] = create_r_field();
+#else
+      qblock[ib]=(Real *)malloc(sizeof(Real)*nt);
+      q2block[ib]=(Real *)malloc(sizeof(Real)*nt);
+      for(int t=0;t<nt;t++){
+	qblock[ib][t]=0;
+	q2block[ib][t]=0;
+      }
+#endif
     }
 
+    /* Read all the data for flavor "jflav" */
+    /* Accumulate the result in qin, weighted by the charge  */
     for(jflav = 0; jflav < param.nflav; jflav++){
-
-      /* Read all the data for flavor "jflav" */
-      /* Accumulate the result in qin, weighted by the charge  */
-
       accumulate_current_density(param.fname_sloppy[jflav], qin_sloppy, param.charges[jflav], 
 				 &param.mass[jflav], param.nrand_sloppy);
-      accumulate_current_density(param.fname_diff[jflav], qin_diff, param.charges[jflav], 
-				 &param.mass[jflav], param.nrand_diff);
+      if(strstr(param.fname_diff[jflav],"none") == NULL)
+	accumulate_current_density(param.fname_diff[jflav], qin_diff, param.charges[jflav], 
+				   &param.mass[jflav], param.nrand_diff);
     }
+
+#ifdef OPT    
+    compute_current_density_and_print(qin_sloppy, param.nrand_sloppy, qin_diff, param.nrand_diff, param.fname_curr);
+#endif
 
     /* Calculate the density-density correlator qblock for each random source blocking size */
     /* Calculate the variance q2block over random source blocks */
@@ -95,20 +117,28 @@ main(int argc, char *argv[])
       destroy_c_array_field(qin_diff[k], NMU);
     free(qin_diff);
 
+#if !defined(TIME_TO_TIME) && !defined(T2TFRMPT2PT)
     /* Symmetrize over hypercubic group transformations */
     for(ib = 0; ib < param.nblock; ib++)
       symmetrize(qblock[ib], q2block[ib]);
-
+#endif
     /* Extrapolate to infinite block size and 
        write the results to the specified file */
     print_result(qblock, q2block, param.nblock, param.block_size);
 
     /* Free qblock array */
     for(ib = 0; ib < param.nblock; ib++){
+#if !defined(TIME_TO_TIME) && !defined(T2TFRMPT2PT)
       destroy_r_field(qblock[ib]);
       destroy_r_field(q2block[ib]);
+#else
+      free(qblock[ib]);
+      free(q2block[ib]);
+#endif	  
     }
+
     free(qblock);
+    free(q2block);
   
   } /* readin(prompt) */
 
