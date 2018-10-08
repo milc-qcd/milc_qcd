3776f18f2cf5e2048f09cd05996c6a0d9fa332b6
Shuhei Yamamoto <shuhei.yamamoto.2011@gmail.com>
2018-10-08 16:06:51 -0700

    add a new function definition.

--- rcorr/print_corr.c
+++ rcorr/print_corr.c
@@ -8,6 +8,8 @@
 
 #include "rcorr_includes.h"
 
+#define FORALLSITESLIN(x,y,z,t,a,b,c,d) for(x=0;x<a;x++)for(y=0;y<b;y++)for(z=0;z<c;z++)for(t=0;t<d;t++)
+
 static FILE *
 open_corr_file(void){
   FILE *fp;
@@ -125,7 +127,6 @@ print_result(Real *q[], Real *q2[], int nblock, int block_size[]){
   }
 
   /* Unbinned output for r <= RMAX */
-  
 
   for(ib = 0; ib < nblock; ib++){
     totmult[ib] = 0;
@@ -256,7 +257,7 @@ print_result(Real *q[], Real *q2[], int nblock, int block_size[]){
 	}
       }
     }
-      
+
   if(this_node == 0){
     close_corr_file(fp);
     for(ib = 0; ib < nblock; ib++)
@@ -303,4 +304,73 @@ print_result_time(Real *q[], Real *q2[], int nblock, int block_size[]){
     
 } /* print_corr.c */
 
+void
+print_result_time2(Real *q[], Real *q2[], int nblock, int block_size[]){
+  double corrvsr[MAXBIN][nblock];
+  double corrvsr2[MAXBIN][nblock];
+  int nvsr[MAXBIN];
+  double rvsr[MAXBIN];
+  double r, myq, dmyq, qtot[nblock];
+  double myqb[nblock], myqb2[nblock];
+  double bsinv[nblock], sd[nblock];
+  double m, dm, chisq;
+  double *c_t = (double *) malloc(sizeof(double)*nt);
+  double *c2_t= (double *) malloc(sizeof(double)*nt);
+  int mult;
+  int totmult[nblock];
+  int i, ib;
+  int x,y,z,t;
+  FILE *fp;
 
+  fp = open_corr_file();
+
+  for(ib = 0; ib < nblock; ib++){
+    bsinv[ib] = 1./((double) block_size[ib]);
+    qtot[ib] = 0.;
+  }
+  
+  for(t=0;t<nt;t++){
+    c_t[t] = 0;
+    c2_t[t]=0;
+  }
+  
+  FORALLSITESLIN(x,y,z,t,nx,ny,nz,nt){
+    for(ib = 0; ib < nblock; ib++){
+      myqb[ib] = 0.;
+      myqb2[ib] = 0.;
+    }
+    if(node_number(x,y,z,t) == this_node){
+      for(ib = 0; ib < nblock; ib++){
+	myqb[ib] = q[ib][node_index(x,y,z,t)];
+	myqb2[ib] = q2[ib][node_index(x,y,z,t)];
+      }
+    }
+    
+    /* Collect values on all nodes by broadcasting the values 
+       at each node to all other nodes (only the values at node 0 
+       needed for printing them) */
+    g_vecdoublesum(myqb, nblock);
+    g_vecdoublesum(myqb2, nblock);
+
+    if(this_node==0){
+      for(ib = 0; ib < nblock; ib++)
+	sd[ib] = sqrt(myqb2[ib]);
+      chisq = linearlsq(&m, &dm, &myq, &dmyq, bsinv, myqb, sd, nblock);
+      node0_printf("%e %e %d %d %d %d\n",myq, dmyq,x,y,z,t);
+      c_t[t]+=myq/3;
+      c2_t[t]+=dmyq*dmyq/9;
+    }
+  }
+  
+  if(this_node == 0){
+    for(t=0;t<nt;t++)
+      fprintf(fp, "%2d %15.8e %15.8e %7.3f \n",t,c_t[t],sqrt(c2_t[t]),0);
+  }
+
+  if(this_node == 0){
+    close_corr_file(fp);
+    for(ib = 0; ib < nblock; ib++)
+      node0_printf("qtot2[%d] = %e  mult = %d\n", ib, qtot[ib], totmult[ib]);
+  }
+  
+} /* print_corr.c */
