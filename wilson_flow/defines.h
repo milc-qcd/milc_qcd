1022567205d71ac4d8a98a1cdb079a6dcb060b17
Alexei Bazavov <bazalesha@gmail.com>
2020-06-29 20:48:37 -0400

    Move definition of flow_step macro here (based on the integrator choice)

--- wilson_flow/defines.h
+++ wilson_flow/defines.h
@@ -33,4 +33,17 @@
 #define SAFETY 0.95
 
 
+/* one step of the flow, here branching into different integrators happens */
+#if GF_INTEGRATOR==INTEGRATOR_LUSCHER || GF_INTEGRATOR==INTEGRATOR_CK
+#define flow_step integrate_RK_2N
+#elif GF_INTEGRATOR==INTEGRATOR_RKMK3
+#define flow_step integrate_RKMK3
+#elif GF_INTEGRATOR==INTEGRATOR_RKMK4 || GF_INTEGRATOR==INTEGRATOR_RKMK5 || GF_INTEGRATOR==INTEGRATOR_RKMK8
+#define flow_step integrate_RKMK_generic
+#elif GF_INTEGRATOR==INTEGRATOR_ADAPT_LUSCHER
+#define flow_step integrate_adapt_RK_2N
+#elif GF_INTEGRATOR==INTEGRATOR_ADAPT_BS
+#define flow_step integrate_adapt_bs
+#endif
+
 #endif /* _DEFINES_H */
