c43a4a7cdebf74ee68c7e9a73a371d83cc6a6ed0
Yin Lin <ylin910095@gmail.com>
2020-07-05 17:01:48 -0500

    Reduce total number of corr and prop - it causes QUDA to stuck with multi-GPU

--- ks_spectrum/params.h
+++ ks_spectrum/params.h
@@ -13,7 +13,7 @@
 #define MAX_MASS_PBP 8
 #define MAX_SOURCE 128
 #define MAX_SET 256
-#define MAX_PROP 128
+#define MAX_PROP 32
 #define MAX_QK 256
 #define MAX_COMBO 8
 #define MAX_PAIR 5000
@@ -25,7 +25,7 @@
 #define MAX_BARYON_LABEL 64
 #define MAX_MESON_MOMENTUM 100
 #define MAX_MOM_LABEL 16
-#define MAX_CORR 2048
+#define MAX_CORR 200
 #define STATIC_TYPE 0
 #define KS_TYPE 1
 #define PROP_TYPE 0
