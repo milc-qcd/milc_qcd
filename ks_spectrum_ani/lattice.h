266c41cfc79d8d24ea0358e34972ac5517b4937b
weberjo8 <dr.rer.nat.weber@gmail.com>
2020-11-30 13:42:09 +0100

    changed data type of volume in ks_spectrum_ani/lattice.h

--- ks_spectrum_ani/lattice.h
+++ ks_spectrum_ani/lattice.h
@@ -63,7 +63,7 @@ typedef struct {
 EXTERN	int nx,ny,nz,nt;	/* lattice dimensions */
 EXTERN  int iseed;
 EXTERN  int niter, nrestart;
-EXTERN  int volume;		/* volume of lattice = nx*ny*nz*nt */
+EXTERN  size_t volume;		/* volume of lattice = nx*ny*nz*nt */
 #ifdef FIX_NODE_GEOM
 EXTERN  int node_geometry[4];  /* Specifies fixed "nsquares" (i.e. 4D
 			    hypercubes) for the nodes in each
