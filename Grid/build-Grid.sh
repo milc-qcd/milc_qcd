12ddd7d9cbc34012f5ac27807529d8da8932fc8b
Carleton DeTar <detar@physics.utah.edu>
2021-02-17 20:19:20 -0700

    First pass at the AMD configure flags, based on the Grid Wiki

--- Grid/build-Grid.sh
+++ Grid/build-Grid.sh
@@ -13,11 +13,11 @@ then
 fi
 
 case ${ARCH} in
-    scalar|avx512-knl|avx512-skx|avx2|gpu-cuda)
+    scalar|avx512-knl|avx512-skx|avx2|gpu-cuda|gpu-hip)
       ;;
     *)
       echo "Unsupported ARCH"
-      echo "Usage $0 <scalar|avx2|avx512-knl|avx512-skx|gpu-cuda> <PK_CC> <PK_CXX>"
+      echo "Usage $0 <scalar|avx2|avx512-knl|avx512-skx|gpu-cuda|gpu-hip> <PK_CC> <PK_CXX>"
       exit 1
 esac
 
@@ -135,9 +135,26 @@ then
         status=$?
         echo "Configure exit status $status"
 	;;
+    gpu-hip)
+	export PATH=/opt/rocm/bin:${PATH}
+	${SRCDIR}/configure \
+             --prefix ${INSTALLDIR}      \
+             --enable-unified=no \
+	     --enable-accelerator=hip \
+	     --enable-comms=mpi3-auto \
+	     --enable-simd=GPU \
+	     --enable-gen-simd-width=64 \
+	     --with-mpfr=${HOME}/mpfr \
+	     --with-lime=${HOME}/scidac/install/qio \
+             --host=x86_64-unknown-linux-gnu \
+	     CXX=hipcc \
+	     MPICXX=mpicxx \
+	     CPPFLAGS="-I/opt/rocm/rocthrust/include" \
+	     LDFLAGS="-L/opt/rocm/rocthrust/lib"
 
-    #              --enable-simd=GEN           \
-
+        status=$?
+        echo "Configure exit status $status"
+	;;
     *)
     echo "Unsupported ARCH ${ARCH}"
           exit 1;
