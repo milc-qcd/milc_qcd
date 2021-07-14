09d8f9ea69011e4288c73b8b88c43748dbad34db
Evan Weinberg <evanweinberg@gmail.com>
2021-07-14 11:06:08 -0400

    Added a compile script for ks_spectrum.

---
+++ ks_spectrum/compile_ks_spectrum_quda.sh
@@ -0,0 +1,33 @@
+
+PATH_TO_CUDA=...
+QUDA_HOME=...
+USQCD_BUILD=...
+
+# Uncomment ARCH, COMPILER, OPT for power9
+# Remove CTIME to remove overly verbose timing output. Useful for debugging.
+
+#ARCH="pow9" \
+#COMPILER="gnu" \
+#OPT="-O3 -Ofast" \
+MY_CC=mpicc \
+MY_CXX=mpicxx \
+CUDA_HOME=${PATH_TO_CUDA} \
+QUDA_HOME=${QUDA_BUILD} \
+WANTQUDA=true \
+WANT_FN_CG_GPU=true \
+WANT_FL_GPU=true \
+WANT_GF_GPU=true \
+WANT_FF_GPU=true \
+WANT_MIXED_PRECISION_GPU=2 \
+PRECISION=2 \
+MPP=true \
+OMP=true \
+WANTQIO=true \
+WANTQMP=true \
+QIOPAR=${USQCD_BUILD} \
+QMPPAR=${USQCD_BUILD} \
+CGEOM="-DFIX_NODE_GEOM -DFIX_IONODE_GEOM" \
+KSCGMULTI="-DKS_MULTICG=HYBRID -DMULTISOURCE -DMULTIGRID" \
+CTIME="-DNERSC_TIME -DCGTIME -DFFTIME -DGFTIME -DREMAP -DPRTIME -DIOTIME" \
+make -j 1 ks_spectrum_hisq
+
