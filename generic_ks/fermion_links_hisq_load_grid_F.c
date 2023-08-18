71fa842dce10fa907d55838fb52f997baaa61506
Carleton DeTar <detar@physics.utah.edu>
2023-08-17 21:53:26 -0600

    Add interface for Grid link fattening

---
+++ generic_ks/fermion_links_hisq_load_grid_F.c
@@ -0,0 +1,11 @@
+/******************** fermion_links_fn_load_grid_F.c ****************************/
+/* MILC Version 7 */
+
+/* For specifically single precision link fattening using Grid routines */
+
+/* 6/2/23 C. DeTar */
+
+#undef GRID_PrecisionInt
+#define GRID_PrecisionInt 1
+
+#include "fermion_links_hisq_load_grid_P.c"
