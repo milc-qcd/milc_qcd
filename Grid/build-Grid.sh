b16e2fac024fddbca403ec801192f91010f38291
Carleton DeTar <detar@physics.utah.edu>
2024-04-18 15:25:21 +0000

    Remove attempt to load stacktrace.

--- Grid/build-Grid.sh
+++ Grid/build-Grid.sh
@@ -210,7 +210,7 @@ then
 	 --enable-unified=no \
 	 MPICXX=mpicxx \
          CXX="${PK_CXX}" CC="${PK_CC}" \
-	 LDFLAGS="-fiopenmp -fsycl -fsycl-device-code-split=per_kernel -fsycl-device-lib=all -lze_loader /home/detar/gecko/compile_feb19/milc_qcd/libraries/stacktrace.o" \
+	 LDFLAGS="-fiopenmp -fsycl -fsycl-device-code-split=per_kernel -fsycl-device-lib=all -lze_loader" \
 	 CXXFLAGS="-fiopenmp -fsycl-unnamed-lambda -fsycl -I/include -Wno-tautological-compare"
 
 	# /soft/compilers/oneapi/2023.12.15.001/oneapi/2024.0/include
