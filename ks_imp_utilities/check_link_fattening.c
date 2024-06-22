4f7a9b2df08fee12d72c8c4b1f2c3e3c6a412569
Carleton DeTar <detar@physics.utah.edu>
2024-06-21 20:31:22 -0600

    Clean up code

--- ks_imp_utilities/check_link_fattening.c
+++ ks_imp_utilities/check_link_fattening.c
@@ -1,7 +1,5 @@
 /********************** check_fermion_force.c *******************************/
 /* MIMD version 7 */
-/* Main procedure for SU3 with dynamical staggered fermions        */
-/* general quark action, general gauge action */
 
 /* This code performs and/or checks the link fattening calculation */
 /* It compares standard MILC link fattening with either a trial input
@@ -23,7 +21,7 @@
 */   
    
 
-#include "ks_imp_includes.h"	/* definitions files and prototypes */
+#include "ks_imp_utilities_includes.h"	/* definitions files and prototypes */
 #if defined(HAVE_QIO) || defined(HAVE_GRID)
 #include <qio.h>
 #else
@@ -40,106 +38,170 @@ void check_link_fattening( char *lngansfile, int lngansflag, char *fatansfile, i
 
   ks_action_paths_hisq *ap = get_action_paths_hisq(&fn_links[0]);
 
-  /* The trusted results */
+  /* Get fat and long link pointers for links generated in main by the
+     code specified in the Makefile. e.g. WANTGRID=true generates them
+     with GRID */
   imp_ferm_links_t *fn = get_fm_links(fn_links)[0];
   su3_matrix *lng = get_lnglinks(fn);
   su3_matrix *fat = get_fatlinks(fn);
   
-  su3_matrix *fattry;
-  su3_matrix *lngtry;
+  /*------------------------------------------------------------*/
+  /* Read fiducial result if given */
+
+  su3_matrix *fattrust;
+  su3_matrix *lngtrust;
   
   if(lngansflag != FRESH && fatansflag != FRESH){
+
     /* If the answer file is given, read it for comparison */
     node0_printf("Reading the fat and long links from files\n"); fflush(stdout);
-    lngtry = (su3_matrix *)malloc(4*sites_on_node*sizeof(su3_matrix));
-    if(lngtry == NULL){
-      node0_printf("No room for lngtry\n");
+    lngtrust = (su3_matrix *)malloc(4*sites_on_node*sizeof(su3_matrix));
+    if(lngtrust == NULL){
+      node0_printf("No room for lngtrust\n");
       terminate(1);
     }
-    memset(lngtry, '\0', 4*sites_on_node*sizeof(su3_matrix));
+    memset(lngtrust, '\0', 4*sites_on_node*sizeof(su3_matrix));
     
-    fattry = (su3_matrix *)malloc(4*sites_on_node*sizeof(su3_matrix));
-    if(fattry == NULL){
-      node0_printf("No room for fattry\n");
+    fattrust = (su3_matrix *)malloc(4*sites_on_node*sizeof(su3_matrix));
+    if(fattrust == NULL){
+      node0_printf("No room for fattrust\n");
       terminate(1);
     }
-    memset(fattry, '\0', 4*sites_on_node*sizeof(su3_matrix));
+    memset(fattrust, '\0', 4*sites_on_node*sizeof(su3_matrix));
 
-    restore_color_matrix_scidac_to_field(lngansfile, lngtry, 4, MILC_PRECISION);
-    restore_color_matrix_scidac_to_field(fatansfile, fattry, 4, MILC_PRECISION);
+    restore_color_matrix_scidac_to_field(lngansfile, lngtrust, 4, MILC_PRECISION);
+    restore_color_matrix_scidac_to_field(fatansfile, fattrust, 4, MILC_PRECISION);
+
   } else {
-    fattry = fat;
-    lngtry = lng;
+
+    fattrust = fat;
+    lngtrust = lng;
+
   }
 
-  node0_printf("Checking the long-link answer\n"); fflush(stdout);
+  /*------------------------------------------------------------*/
+  if(lngansflag != FRESH){
 
-  Real maxdiff = 0;
-  Real maxnorm = 0;
+    node0_printf("Checking the long-link answer\n"); fflush(stdout);
 
-  int i, dir;
-  FORALLFIELDSITES(i){
-    FORALLUPDIR(dir){
-      su3_matrix diffmat;
-      su3_matrix *tmat = lng + 4*i + dir;
-      sub_su3_matrix( lngtry + 4*i + dir, tmat, &diffmat);
-      Real diff = sqrt(realtrace_su3( &diffmat, &diffmat ));
-      Real norm = sqrt(realtrace_su3( tmat, tmat));
-      if(diff > tol * norm){
-	printf("Large relative difference %e node %d coord %d %d %d %d dir %d\n",
-	       diff/norm,this_node,
-	       lattice[i].x,lattice[i].y,lattice[i].z,lattice[i].t,dir);
-	
-	printf("From file\n");
-	dumpmat(lngtry + 4*i + dir);
-	printf("From link calculation\n");
-	dumpmat(tmat);
+    Real maxdiff = 0;
+    Real maxnorm = 0;
+    
+    int i, dir;
+    FORALLFIELDSITES(i){
+      FORALLUPDIR(dir){
+	su3_matrix diffmat;
+	su3_matrix *tmat = lng + 4*i + dir;
+	sub_su3_matrix( lngtrust + 4*i + dir, tmat, &diffmat);
+	Real diff = sqrt(realtrace_su3( &diffmat, &diffmat ));
+	Real norm = sqrt(realtrace_su3( tmat, tmat));
+	if(diff > tol * norm){
+	  printf("Large relative difference %e node %d coord %d %d %d %d dir %d\n",
+		 diff/norm,this_node,
+		 lattice[i].x,lattice[i].y,lattice[i].z,lattice[i].t,dir);
+	  
+	  printf("From file\n");
+	  dumpmat(lngtrust + 4*i + dir);
+	  printf("From link calculation\n");
+	  dumpmat(tmat);
+	}
+	if(maxdiff < diff)maxdiff = diff;
+	if(maxnorm < norm)maxnorm = norm;
       }
-      if(maxdiff < diff)maxdiff = diff;
-      if(maxnorm < norm)maxnorm = norm;
+    }
+    
+    g_floatmax(&maxdiff);
+    g_floatmax(&maxnorm);
+    if(maxnorm > 0){
+      Real reldiff = maxdiff/maxnorm;
+      node0_printf("Relative difference %e\n",reldiff);
     }
   }
 
-  g_floatmax(&maxdiff);
-  g_floatmax(&maxnorm);
-  if(maxnorm > 0){
-    Real reldiff = maxdiff/maxnorm;
-    node0_printf("Relative difference %e\n",reldiff);
-  }
+  /*------------------------------------------------------------*/
+  if(fatansflag != FRESH){
 
-
-  node0_printf("Checking the fat-link answer\n"); fflush(stdout);
-
-  maxdiff = 0;
-  maxnorm = 0;
-
-  FORALLFIELDSITES(i){
-    FORALLUPDIR(dir){
-      su3_matrix diffmat;
-      su3_matrix *tmat = fat + 4*i + dir;
-      sub_su3_matrix( fattry + 4*i + dir, tmat, &diffmat);
-      Real diff = sqrt(realtrace_su3( &diffmat, &diffmat ));
-      Real norm = sqrt(realtrace_su3( tmat, tmat));
-      if(diff > tol * norm){
-	printf("Large relative difference %e node %d coord %d %d %d %d dir %d\n",
-	       diff/norm,this_node,
-	       lattice[i].x,lattice[i].y,lattice[i].z,lattice[i].t,dir);
-	printf("From file\n");
-	dumpmat(fattry + 4*i + dir);
-	printf("From link calculation\n");
-	dumpmat(tmat);
+    node0_printf("Checking the fat-link answer\n"); fflush(stdout);
+    
+    maxdiff = 0;
+    maxnorm = 0;
+    
+    FORALLFIELDSITES(i){
+      FORALLUPDIR(dir){
+	su3_matrix diffmat;
+	su3_matrix *tmat = fat + 4*i + dir;
+	sub_su3_matrix( fattrust + 4*i + dir, tmat, &diffmat);
+	Real diff = sqrt(realtrace_su3( &diffmat, &diffmat ));
+	Real norm = sqrt(realtrace_su3( tmat, tmat));
+	if(diff > tol * norm){
+	  printf("Large relative difference %e node %d coord %d %d %d %d dir %d\n",
+		 diff/norm,this_node,
+		 lattice[i].x,lattice[i].y,lattice[i].z,lattice[i].t,dir);
+	  printf("From file\n");
+	  dumpmat(fattrust + 4*i + dir);
+	  printf("From link calculation\n");
+	  dumpmat(tmat);
+	}
+	if(maxdiff < diff)maxdiff = diff;
+	if(maxnorm < norm)maxnorm = norm;
       }
-      if(maxdiff < diff)maxdiff = diff;
-      if(maxnorm < norm)maxnorm = norm;
     }
+    
+    g_floatmax(&maxdiff);
+    g_floatmax(&maxnorm);
+    if(maxnorm > 0){
+      Real reldiff = maxdiff/maxnorm;
+      node0_printf("Relative difference %e\n",reldiff);
+    } else {
+      node0_printf("Absolute difference %e but maxnorm is 0???\n",maxdiff);
+    }      
   }
-
-  g_floatmax(&maxdiff);
-  g_floatmax(&maxnorm);
-  if(maxnorm > 0){
-    Real reldiff = maxdiff/maxnorm;
-    node0_printf("Relative difference %e\n",reldiff);
-  } else {
-    node0_printf("Absolute difference %e but maxnorm is 0???\n",maxdiff);
-  }      
+  
+  /*------------------------------------------------------------*/
+  /* save longlinks if requested */
+  if (savelongflag != FORGET ){
+#ifdef HAVE_QIO
+    su3_matrix *lng = get_lnglinks(fn);
+    char *filexml = create_QCDML();
+    if(!param.withKSphases){
+      rephase_field_offset( lng, OFF, NULL, param.coord_origin);
+      node0_printf("Saving the long links with KS phases OUT buttime BC IN\n");
+    } else {
+      node0_printf("Saving the long links with KS phases IN and time BC IN\n");
+    }
+    node0_printf("Saving the long links with LFN\n '%s'\n", stringLFNlong);
+    save_color_matrix_scidac_from_field( savelongfile, filexml, 
+					 "Long links", QIO_SINGLEFILE, lng, 4, MILC_PRECISION,
+					 stringLFNlong);
+    free_QCDML(filexml);
+    if(!param.withKSphases)
+      rephase_field_offset( lng, ON, NULL, param.coord_origin);
+#else
+    printf("ERROR: Can't save the longlinks.  Recompile with QIO\n");
+#endif
+  }
+  
+  /*------------------------------------------------------------*/
+  /* save fat links if requested */
+  if (savefatflag != FORGET ){
+#ifdef HAVE_QIO
+    su3_matrix *fat = get_fatlinks(fn);
+    char *filexml = create_QCDML();
+    if(!param.withKSphases){
+      rephase_field_offset( fat, OFF, NULL, param.coord_origin);
+      node0_printf("Saving the fat links with KS phases OUT but time BC IN\n");
+    } else {
+      node0_printf("Saving the fat links with KS phases IN and time BC IN\n");
+    }
+    node0_printf("Saving the fat links with LFN\n '%s'\n", stringLFNfat);
+    save_color_matrix_scidac_from_field( savefatfile, filexml, 
+					 "Fat links", QIO_SINGLEFILE, fat, 4, MILC_PRECISION,
+					 stringLFNfat);
+    free_QCDML(filexml);
+    if(!param.withKSphases)
+      rephase_field_offset( fat, ON, NULL, param.coord_origin);
+#else
+    printf("ERROR: Can't save the fatlinks.  Recompile with QIO\n");
+#endif
+  }
 }      
