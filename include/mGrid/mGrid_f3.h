40b89255ac4784727c963a9448fbb7284038c2fb
Carleton DeTar <detar@physics.utah.edu>
2025-07-15 14:02:16 -0600

    Change *multi_x type to *multi_x[] type

--- include/mGrid/mGrid_f3.h
+++ include/mGrid/mGrid_f3.h
@@ -134,7 +134,7 @@ void GRID_F3_reunit_deriv(GRID_info_t *info, su3_matrix *V, su3_matrix *dW,
 void GRID_F3_hisq_force(GRID_info_t *info,
 			void *fl,
 			float residues[],
-			su3_vector *multi_x,
+			su3_vector *multi_x[],
 			int n_orders_naik[],
 			su3_matrix *deriv,
 			GRID_4Dgrid *grid_full);
