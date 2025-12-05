39a553465191f8249b841305224099988e1a2a86
Carleton DeTar <detar@physics.utah.edu>
2025-12-05 15:12:38 -0500

    Restore ifdefs

--- generic_ks/d_congrad5_fn_quda.c
+++ generic_ks/d_congrad5_fn_quda.c
@@ -130,8 +130,7 @@ int ks_congrad_parity_gpu(su3_vector *t_src, su3_vector *t_dest,
   inv_args.tadpole = u0;
 #endif
 
-  //#if !( defined(USE_CG_GPU) && defined(HAVE_QUDA) && defined(USE_EIG_GPU) )
-#if 1   // Temporary
+#if !( defined(USE_CG_GPU) && defined(HAVE_QUDA) && defined(USE_EIG_GPU) )
 
   // Inversion without deflation and eigensolve on GPU
 
@@ -368,8 +367,7 @@ int ks_congrad_block_parity_gpu(int nsrc, su3_vector **t_src, su3_vector **t_des
   inv_args.tadpole = u0;
 #endif
 
-  //#if !( defined(USE_CG_GPU) && defined(HAVE_QUDA) && defined(USE_EIG_GPU) )
-#if 1  // Temporary
+#if !( defined(USE_CG_GPU) && defined(HAVE_QUDA) && defined(USE_EIG_GPU) )
 
   // Inversion without deflation and eigensolve on GPU
 
