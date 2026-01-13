4ec7a2894f3b31f74343b714282251e49852e162
Carleton DeTar <detar@physics.utah.edu>
2026-01-13 16:28:43 -0500

    Require eigenvectors/values to be double precision

--- ks_spectrum/lattice.h
+++ ks_spectrum/lattice.h
@@ -143,7 +143,8 @@ EXTERN Real g_splaq,g_tplaq;	/* global U(1) plaquette measures */
 /* For eigenpair calculation */
 EXTERN int Nvecs_tot;
 EXTERN int Nvecs_alloc;
-EXTERN Real *eigVal; /* eigenvalues of D^dag D */
-EXTERN su3_vector **eigVec; /* eigenvectors */
+EXTERN double *eigVal; /* eigenvalues of D^dag D */
+EXTERN dsu3_vector **eigVec; /* eigenvectors */
+EXTERN int eigenvectors_offloaded;
 
 #endif /* _LATTICE_H */
