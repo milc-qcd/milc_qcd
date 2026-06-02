bf87d71c7593b7c183542a8be741915e11b04f97
Carleton DeTar <detar@physics.utah.edu>
2026-06-02 11:31:03 -0600

    Hard wire double precision "momentum"

--- generic_ks/fermion_force_hisq_multi_grid.c
+++ generic_ks/fermion_force_hisq_multi_grid.c
@@ -43,7 +43,7 @@ fermion_force_multi_hisq_grid(info_t* info, int prec, Real eps, Real *residues,
   double *epsv = (double *)malloc(sizeof(double)*nterms);
   for(int i = 0; i < nterms; i++) epsv[i] = eps*residues[i];
 
-  dsu3_matrix* momentum = (su3_matrix *)malloc(sites_on_node*4*sizeof(dsu3_matrix));
+  dsu3_matrix* momentum = (dsu3_matrix *)malloc(sites_on_node*4*sizeof(dsu3_matrix));
 
   /* Support only double-precision calculation */
   GRID_D3_hisq_force(&grid_info, fl, epsv, multi_x, n_orders_naik, momentum,
